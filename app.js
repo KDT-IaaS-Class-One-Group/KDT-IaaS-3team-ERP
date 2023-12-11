7145c005c199212c619970470b42df4591ebeb63
Ho Nyeong <dev.honing@gmail.com>
2023-12-11 17:48:44 +0900

    :bulb: Comment: app.js 주석 수정

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js - 서버 생성
+// app.js: 서버 생성
 
 const express = require('express');
 const path = require('path');
@@ -6,12 +6,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// 정적 파일 서빙 - public 폴더
+// 정적 파일 서빙: public 폴더
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 루트 경로로 접속했을 때 index.html 응답
+// index.html을 서빙하기 위한 라우팅
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.send(path.join(__dirname, 'index.html'));
 });
 
 // 서버 시작
