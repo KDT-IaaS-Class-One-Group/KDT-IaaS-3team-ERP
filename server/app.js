300b1c65194726ae14d318557f4704595449384b
eunjeong <eunjeong990731@gmail.com>
2023-12-12 10:02:20 +0900

    :poop: Bad: json 가져오기
    실행 안되는듯

--- server/app.js
+++ server/app.js
@@ -10,37 +10,37 @@ const path= require('path')
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-// // JSON 파일에서 데이터를 읽어옴
-// function readProducts() {
-//     const rawData = fs.readFileSync('products.json');
-//     return JSON.parse(rawData);
-// }
+// JSON 파일에서 데이터를 읽어옴
+function readProducts() {
+    const rawData = fs.readFileSync('products.json');
+    return JSON.parse(rawData);
+}
 
-// // JSON 파일에 데이터를 저장
-// function saveProducts(data) {
-//     const jsonData = JSON.stringify(data, null, 2);
-//     fs.writeFileSync('products.json', jsonData);
-// }
+// JSON 파일에 데이터를 저장
+function saveProducts(data) {
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFileSync('products.json', jsonData);
+}
 
-// app.get('/getProducts', (req, res) => {
-//     const products = readProducts();
-//     res.json(products);
-// });
+app.get('/getProducts', (req, res) => {
+    const products = readProducts();
+    res.json(products);
+});
 
-// app.post('/addProduct', (req, res) => {
-//     const newProduct = req.body;
+app.post('/addProduct', (req, res) => {
+    const newProduct = req.body;
 
-//     // 서버에서 데이터 읽어오기
-//     const products = readProducts();
+    // 서버에서 데이터 읽어오기
+    const products = readProducts();
 
-//     // 새 상품 추가
-//     products.push(newProduct);
+    // 새 상품 추가
+    products.push(newProduct);
 
-//     // 데이터 저장
-//     saveProducts(products);
+    // 데이터 저장
+    saveProducts(products);
 
-//     res.send('Product added successfully!');
-// });
+    res.send('Product added successfully!');
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
