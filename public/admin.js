ab3f7405a8f4aa5f977180c86637bfb22fffb8dd
eunjeong <eunjeong990731@gmail.com>
2023-12-12 17:50:48 +0900

    :poop: Bad: 상품 등록 함수 변경
    json으로 보낼때 빈객체로 내보내지 않으려 했는데 빈객체로 내보내네 하아...

--- public/admin.js
+++ public/admin.js
@@ -16,7 +16,6 @@ function displayAdminPage(id) {
     `;
 }
 
-// 추가된 부분: 상품 등록 함수
 window.addProduct = function() {
     const imageInput = document.getElementById('imageInput');
     const productName = document.getElementById('productName').value;
@@ -24,21 +23,44 @@ window.addProduct = function() {
 
     // 이미지 업로드 및 상품 정보 서버로 전송
     const formData = new FormData();
-    formData.append('image', imageInput.files[0]);
+    formData.append('img', imageInput.files[0]);
     formData.append('name', productName);
     formData.append('description', productDescription);
+    
 
     fetch('/addProduct', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json()) // 서버에서 JSON 형식으로 응답하도록 수정
-    .then(products => {
-        // 등록된 상품 정보를 유저 페이지에 업데이트
-        displayUserPage(products);
-    })
-    .catch(error => {
-        console.error(error);
-        // 에러 메시지 표시 등 추가적인 처리
+    .then(response => response.text())
+    .then(message => {
+        // 등록 결과 메시지 표시
+        document.getElementById('resultMessage').innerText = message;
+        // 상품 목록 다시 표시
+        displayProducts();
     });
 };
+
+
+function displayProducts() {
+    fetch('/getProducts')
+        .then(response => response.json())
+        .then(products => {
+            const productsContainer = document.getElementById('productsContainer');
+            productsContainer.innerHTML = '';
+
+            products.forEach(product => {
+                const productDiv = document.createElement('div');
+                productDiv.innerHTML = `
+                    <img src="${product.image}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                    <p>${product.description}</p>
+                `;
+                productsContainer.appendChild(productDiv);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+            // 에러 메시지 표시 등 추가적인 처리
+        });
+}
