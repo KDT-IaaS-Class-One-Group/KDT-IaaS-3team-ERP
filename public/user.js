14a4d9bae9d10818f646f271c6bee2c5d5943b70
eunjeong <eunjeong990731@gmail.com>
2023-12-12 10:01:54 +0900

    :poop: Bad: 어드민에서 등록한 상품 가져오는 함수 작성
    실행은 안됨

--- public/user.js
+++ public/user.js
@@ -11,6 +11,29 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function displayUserPage(id) {
-    document.getElementById('content').innerHTML = `<h2>Welcome User ${id}!</h2>`;
+    document.getElementById('content').innerHTML = `<h2>Welcome ${id}!</h2>`;
     // 사용자 페이지에 필요한 내용을 추가할 수 있습니다.
 }
+
+// 추가된 부분: 상품 표시 함수
+function displayProducts(products) {
+    const productsContainer = document.getElementById('productsContainer');
+    productsContainer.innerHTML = '';
+
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.innerHTML = `
+            <img src="${product.image}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <p>${product.description}</p>
+        `;
+        productsContainer.appendChild(productDiv);
+    });
+}
+
+// 서버에서 상품 정보 가져와서 표시
+fetch('/getProducts')
+    .then(response => response.json())
+    .then(products => {
+        displayProducts(products);
+    });
