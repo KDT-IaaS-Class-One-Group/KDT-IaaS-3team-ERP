82acc8b1237176a187980a084638f224e14f986c
eunjeong <eunjeong990731@gmail.com>
2023-12-12 17:49:46 +0900

    :hammer: Modify: 파일 경로를 변수로 선언
    안정성을 위해 코드 경로 변수로 지정

--- server/app.js
+++ server/app.js
@@ -7,49 +7,44 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json()); // JSON 파싱을 위한 미들웨어 추가
 
+// JSON 파일 경로 설정
+const productsFilePath = path.join(__dirname, './products.json');
+
 // JSON 파일에서 데이터를 읽어옴
 function readProducts() {
-    const rawData = fs.readFileSync(path.join(__dirname, './producrs.json'));
+    const rawData = fs.readFileSync(productsFilePath, 'utf8');
     return JSON.parse(rawData);
 }
 
 // JSON 파일에 데이터를 저장
 function saveProducts(data) {
     const jsonData = JSON.stringify(data, null, 2);
-    fs.writeFileSync(path.join(__dirname, 'products.json'), jsonData);
+    fs.writeFileSync(productsFilePath, jsonData, 'utf8');
 }
 
 app.get('/getProducts', (req, res) => {
-    try {
-        const products = readProducts();
-        res.json(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    const products = readProducts();
+    res.json(products);
 });
 
 app.post('/addProduct', (req, res) => {
-    try {
-        const newProduct = req.body;
+    const newProduct = req.body;
 
-        // 서버에서 데이터 읽어오기
-        const products = readProducts();
-
-        // 새 상품 추가
-        products.push(newProduct);
-
-        // 데이터 저장
-        saveProducts(products);
-
-        // 등록된 상품 정보를 클라이언트에 응답
-        res.json(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+    if (!newProduct.img || !newProduct.name || !newProduct.description) {
+        return res.status(400).send('Invalid product data');
     }
-});
 
+    // 서버에서 데이터 읽어오기
+    const products = readProducts();
+
+    // 새 상품 추가
+    products.push(newProduct);
+
+    // 데이터 저장
+    saveProducts(products);
+
+    res.send('Product added successfully!');
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
