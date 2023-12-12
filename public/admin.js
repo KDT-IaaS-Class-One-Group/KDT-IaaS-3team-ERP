89f49c148c7235276155756adcd6130b10f4f70e
eunjeong <eunjeong990731@gmail.com>
2023-12-12 09:58:54 +0900

    :sparkles: Feat: addProduct 기능추가
    이미지,이름,설명 id json 패치 써서 가져오기

--- public/admin.js
+++ public/admin.js
@@ -12,11 +12,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function displayAdminPage(id) {
     document.getElementById('content').innerHTML = `
-        <h2>Welcome Admin ${id}!</h2>
+        <h2>Welcome ${id}!</h2>
         <button onclick="addProduct()">Add Product</button>
     `;
 }
 
-function addProduct() {
-    // 이전 코드와 동일
-}
+// 추가된 부분: 상품 등록 함수
+window.addProduct = function() {
+    const imageInput = document.getElementById('imageInput');
+    const productName = document.getElementById('productName').value;
+    const productDescription = document.getElementById('productDescription').value;
+
+    // 이미지 업로드 및 상품 정보 서버로 전송
+    const formData = new FormData();
+    formData.append('image', imageInput.files[0]);
+    formData.append('name', productName);
+    formData.append('description', productDescription);
+
+    fetch('/addProduct', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(message => {
+        // 등록 결과 메시지 표시
+        document.getElementById('resultMessage').innerText = message;
+    });
+};
