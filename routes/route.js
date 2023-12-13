41140b632c630e2ba9b1c2b27f1f1d2812ed5e08
Ho Nyeong <dev.honing@gmail.com>
2023-12-14 00:24:23 +0900

    :sparkles: Feat: 라우팅 처리를 통해 폼 데이터를 userInfo.json에 저장
    - 사용자, root, 1234
    - 관리자, adroot, 1234

--- routes/route.js
+++ routes/route.js
@@ -74,6 +74,37 @@ router.get('/product', (req, res) => {
 router.get('/signup', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/signup.html'));
 });
+// 회원가입 폼 처리
+router.post('/signup', express.urlencoded({ extended: true }), async (req, res) => {
+  // 클라이언트에서 전송한 폼 데이터를 req.body에서 추출
+  const { name, id, pw } = req.body;
+
+  // 모든 필드가 제대로 입력되었는지 확인
+  if (!name || !id || !pw) {
+    // 필드 중 하나라도 누락된 것이 있다면 클라이언트에게 오류 응답 전송
+    return res.status(400).json({ error: '모든 필드를 입력하세요.' });
+  }
+
+  try {
+    // 기존 유저 정보 로드
+    const userInfoPath = path.join(__dirname, '../public/userInfo.json');
+    const data = await fs.readFile(userInfoPath, 'utf-8');
+    const existingUsers = JSON.parse(data);
+
+    // 새로운 유저 정보 추가
+    const newUser = { name, id, pw };
+    existingUsers.push(newUser);
+
+    // 정보를 파일에 저장
+    await fs.writeFile(userInfoPath, JSON.stringify(existingUsers, null, 2), 'utf-8');
+
+    // 회원가입이 성공적으로 완료되었음을 클라이언트에게 응답
+    res.json({ success: true });
+  } catch (error) {
+    console.error('회원가입 중 오류 발생:', error);
+    res.status(500).json({ error: '서버 오류' });
+  }
+});
 
 
 module.exports = router;
