fc661a630f041f8330aecbbf0a9bdc74d29df971
eunjeong <eunjeong990731@gmail.com>
2023-12-13 10:02:06 +0900

    :poop: Bad: img를 image로 변경
    그래도 실행안되는건똑같구연

--- public/admin.js
+++ public/admin.js
@@ -13,23 +13,36 @@ document.addEventListener('DOMContentLoaded', function () {
 function displayAdminPage(id) {
     document.getElementById('productsContainer').innerHTML = `
         <h2>Welcome ${id}!</h2>
+        <form id="productForm">
+            <input type="file" id="imageInput" accept="image/*">
+            <input type="text" id="productName" placeholder="Product Name">
+            <textarea id="productDescription" placeholder="Product Description"></textarea>
+            <button type="button" id="addProductBtn">Add Product</button>
+        </form>
+        <div id="resultMessage"></div>
+        <div id="productsContainer"></div>
     `;
+    // 상품 목록 표시
+    displayProducts();
+
+    // 이벤트 리스너 등록
+    document.getElementById('addProductBtn').addEventListener('click', addProduct);
 }
 
-window.addProduct = function() {
+// 상품 등록 함수
+function addProduct() {
     const imageInput = document.getElementById('imageInput');
     const productName = document.getElementById('productName').value;
     const productDescription = document.getElementById('productDescription').value;
 
     // 이미지 업로드 및 상품 정보 서버로 전송
     const formData = new FormData();
-    formData.append('img', imageInput.files[0]);
+    formData.append('image', imageInput.files[0]);
     formData.append('name', productName);
     formData.append('description', productDescription);
-    
 
     fetch('/addProduct', {
-        method: 'POST',
+        method: 'POST',  // POST 메서드로 변경
         body: formData
     })
     .then(response => response.text())
@@ -39,9 +52,9 @@ window.addProduct = function() {
         // 상품 목록 다시 표시
         displayProducts();
     });
-};
+}
 
-
+// 상품 목록 표시 함수
 function displayProducts() {
     fetch('/getProducts')
         .then(response => response.json())
