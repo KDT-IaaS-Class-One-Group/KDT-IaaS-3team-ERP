0683676933d252947a53d404b5bead1811770ae1
eunjeong <eunjeong990731@gmail.com>
2023-12-14 09:13:44 +0900

    :poop: Bad: network오류

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,3 @@
-const { json } = require("body-parser");
-
 document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
@@ -43,13 +41,21 @@ function addProduct() {
     formData.append('name', productName);
     formData.append('description', productDescription);
 
+    // 상품 등록 함수
+function addProduct() {
+    const imageInput = document.getElementById('imageInput');
+    const productName = document.getElementById('productName').value;
+    const productDescription = document.getElementById('productDescription').value;
+
+    // 이미지 업로드 및 상품 정보 서버로 전송
+    const formData = new FormData();
+    formData.append('image', imageInput.files[0]);
+    formData.append('name', productName);
+    formData.append('description', productDescription);
+
     fetch('/addProduct', {
         method: 'POST',
-        body: json.stringify,
-        headers: {
-            // 이 부분을 추가하여 Content-Type을 명시적으로 설정합니다.
-            'Content-Type': 'application/json',
-        },
+        body: formData,
     })
     .then(response => response.text())
     .then(message => {
@@ -59,6 +65,7 @@ function addProduct() {
         displayProducts();
     });
 }
+
 
 // 상품 목록 표시 함수
 function displayProducts() {
@@ -82,4 +89,4 @@ function displayProducts() {
             console.error(error);
             // 에러 메시지 표시 등 추가적인 처리
         });
-}
+}}
