9a7d5c843e7857c58484c12f5d3d462302466db0
Yusmin97 <tmdamsi97@gmail.com>
2023-12-14 11:42:52 +0900

    ✨ Feat: data.json 데이터 값을 삭제하는 post 라우팅
    data.json 안의 key 값인 item을 삭제하는 post 라우팅

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 // 장바구니 페이지 라우팅
 app.get('/cart', (req, res) => {
   res.sendFile(__dirname + '/publics/cart.html');
-})
+});
 
 // 데이터 저장 및 읽기
 const dataPath = __dirname + '/publics/data.json';
@@ -48,6 +48,26 @@ app.post('/add-to-cart', (req, res) => {
   res.json({ success: true, message: 'Item added to cart successfully' });
 });
 
+// cart.html에 작성된 data.json 값 삭제
+app.post('/delete-items', (req, res) => {
+  try {
+    const itemsToDelete = req.body.items;
+    const cartData = loadData();
+    console.log('Items to delete:', itemsToDelete);
+
+    // 체크된 아이템 삭제
+    const updatedData = cartData.filter((item) => !itemsToDelete.includes(item.item)); // item.item 부분은 value가 아닌 json의 key 값을 설정해줘야 한다.
+
+    saveData(updatedData);
+
+    // 데이터가 성공적으로 삭제가 되면 아래의 문구가 나타난다.
+    res.json({ success: true, message: 'Selected items deleted successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 포트에서 실행 중입니다.`)
-})
+  console.log(`서버가 http://localhost:${port} 포트에서 실행 중입니다.`);
+});
