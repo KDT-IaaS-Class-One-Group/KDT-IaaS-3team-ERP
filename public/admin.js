d47cc91650df4ca85852541ea64e5e5083f675d1
eunjeong <eunjeong990731@gmail.com>
2023-12-14 09:42:32 +0900

    :hammer: Modify: 코드 위치 변경
    코드 위치 변경으로 오류 수정

--- public/admin.js
+++ public/admin.js
@@ -10,6 +10,31 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+
+// 상품 목록 표시 함수
+function displayProducts() {
+    fetch('/getProducts')
+        .then(response => response.json())
+        .then(products => {
+            const productsContainer = document.getElementById('productsContainer');
+            productsContainer.innerHTML = '';
+
+            products.forEach(product => {
+                const productDiv = document.createElement('div');
+                productDiv.innerHTML = `
+                    <img src="${product.image}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                    <p>${product.description}</p>
+                `;
+                productsContainer.appendChild(productDiv);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+            // 에러 메시지 표시 등 추가적인 처리
+        });
+}
+
 function displayAdminPage(id) {
     document.getElementById('productsContainer').innerHTML = `
         <h2>Welcome ${id}!</h2>
@@ -66,27 +91,4 @@ function addProduct() {
     });
 }
 
-
-// 상품 목록 표시 함수
-function displayProducts() {
-    fetch('/getProducts')
-        .then(response => response.json())
-        .then(products => {
-            const productsContainer = document.getElementById('productsContainer');
-            productsContainer.innerHTML = '';
-
-            products.forEach(product => {
-                const productDiv = document.createElement('div');
-                productDiv.innerHTML = `
-                    <img src="${product.image}" alt="${product.name}">
-                    <h3>${product.name}</h3>
-                    <p>${product.description}</p>
-                `;
-                productsContainer.appendChild(productDiv);
-            });
-        })
-        .catch(error => {
-            console.error(error);
-            // 에러 메시지 표시 등 추가적인 처리
-        });
-}}
+}
