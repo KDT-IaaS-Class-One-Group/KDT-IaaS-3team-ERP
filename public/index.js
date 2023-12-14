8f56f7cc210ddee32a96a6187c522b52082366b2
eunjeong <eunjeong990731@gmail.com>
2023-12-14 11:28:13 +0900

    :hammer: Modify: 들여쓰기

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
 function login() {
-    const id = document.getElementById('id').value;
-    const password = document.getElementById('password').value;
+  const id = document.getElementById('id').value;
+  const password = document.getElementById('password').value;
 
-    if (id === 'adroot' && password === '1234') {
-        window.location.href = `admin.html?id=${id}`;
-    } else if (id === 'root' && password === '1234') {
-        window.location.href = `user.html?id=${id}`;
-    } else {
-        alert('Invalid credentials. Please try again.');
-    }
+  if (id === 'adroot' && password === '1234') {
+    window.location.href = `admin.html?id=${id}`;
+  } else if (id === 'root' && password === '1234') {
+    window.location.href = `user.html?id=${id}`;
+  } else {
+    alert('Invalid credentials. Please try again.');
+  }
 }
