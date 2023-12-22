95e91acd290ceda35c14013fa2936d2961361dba
Ho Nyeong <dev.honing@gmail.com>
2023-12-22 16:53:08 +0900

    :sparkles: Feat: 로그인, 회원가입 페이지 연결
    - GET 요청 처리 로직을 추가해 login.html과 signup.html 추가로 서빙

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ const port = 3000;
 // 정적 파일 제공을 위한 미들웨어 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 기본 라우팅
-app.get((req, res) => {
+// GET 요청 처리
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.html 서빙
 })
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html')); // login.html 서빙
+})
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html')); // signup.html 서빙
+})
+
 
 // 서버 시작
 app.listen(port, () => {
