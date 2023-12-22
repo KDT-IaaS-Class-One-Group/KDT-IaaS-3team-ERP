80dc568703aa68595324f253badf613f77328581
Ho Nyeong <dev.honing@gmail.com>
2023-12-22 15:34:53 +0900

    :sparkles: Feat: HTML 파일 서빙 완료
    - public/index.html을 생성
    - 라우팅 로직을 추가해 index.html을 서빙

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // app.js
 
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
+
+// 정적 파일 제공을 위한 미들웨어 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 기본 라우팅
+app.get((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.html 서빙
+})
 
 // 서버 시작
 app.listen(port, () => {
