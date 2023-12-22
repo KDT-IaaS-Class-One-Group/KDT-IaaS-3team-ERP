d8392b15e4129b0305692e32a43f2f77c8249ab9
samuel21695 <samuel21695@gmail.com>
2023-12-22 16:48:08 +0900

    ✨ Feat: login page routing & basic layouts

--- routes/route.js
+++ routes/route.js
@@ -5,10 +5,15 @@ const router = express.Router();
 const fs = require("fs")
 
 // routing
+// main page
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + '/../public/main.html'))
 })
 
+// login page
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + '/../public/login.html'))
+})
 
 // Exporting the module
 module.exports = router;
