13eda6ef462336d2ed7ea2404761188ffbfbc1a2
ChaeYi <chaeyi990731@gmail.com>
2023-12-26 11:42:29 +0900

    :poop: Bad: js파일 서빙 오류
    content-type 연결이 안된다...

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,34 +1,37 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
-var path_1 = __importDefault(require("path"));
-var app = (0, express_1.default)();
-var port = 9997;
-// 정적 파일 서빙 설정 (express.static을 앞쪽으로 이동)
-app.use(express_1.default.static(path_1.default.join(__dirname, '../../public'), {
-    setHeaders: function (res, path, stat) {
-        if (path.endsWith('.js')) {
-            res.setHeader('Content-Type', 'application/javascript');
-        }
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 9997;
+
+// 정적 파일 서빙 설정 수정
+app.use(
+  express.static(path.join(__dirname, '../../dist'), {
+    mimeTypes: {
+      js: 'application/javascript',
     },
-}));
-app.use(body_parser_1.default.json());
-app.get('/', function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, '../../public/mainPage.html'));
+  })
+);
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/mainPage.html'));
 });
-app.get('/login', function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, '../../public/loginPage.html'));
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/loginPage.html'));
 });
-app.get('/signup', function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, '../../public/signupPage.html'));
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/signupPage.html'));
 });
-app.get('/admin', function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, '../../public/adminPage.html'));
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/adminPage.html'));
 });
-app.listen(port, function () {
-    console.log("Server is running at http://localhost:".concat(port));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -5,9 +5,9 @@ import path from 'path';
 const app = express();
 const port = 9997;
 
-// 정적 파일 서빙 설정 (express.static을 앞쪽으로 이동)
+// 정적 파일 서빙 설정 수정
 app.use(
-  express.static(path.join(__dirname, '../../public'), {
+  express.static(path.join(__dirname, '../../dist'), {
     setHeaders: (res, path, stat) => {
       if (path.endsWith('.js')) {
         res.setHeader('Content-Type', 'application/javascript');
