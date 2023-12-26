d3ce0c7f396e3b26651c4b3eb4746096407fbdbe
samuel21695 <samuel21695@gmail.com>
2023-12-26 12:49:49 +0900

    ✨ Feat: try to connect with Maria DB with server

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // Importing modules 
 const express = require('express');
-const path = require('path')
-const route = require('./routes/route')
+const path = require('path');
+const route = require('./routes/route');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
+
+// Connect MariaDB
+const connection = mysql.createConnection({
+  host: 3333,
+  user: "root",
+  password: "Andpf445169!",
+  database: "janjanbari",
+})
 
 // Middleware for serving static files
 app.use(express.static(path.join(__dirname, 'public')));
