dd3cd84d3118e31fe47f17583b7e806ef0fe9b40
ChaeYi <chaeyi990731@gmail.com>
2023-12-26 12:48:31 +0900

    :hammer: Modify : 회원가입 로직
    js 정적파일 서빙 주석 처리, post로 database 저장하기

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,20 +1,21 @@
+// server.ts
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { query, addUser } from '../database/db'; // data.ts 파일에서 export한 함수들을 불러옴
 
 const app = express();
 const port = 9997;
 
-// 정적 파일 서빙 설정 수정
-app.use(
-  express.static(path.join(__dirname, '../../dist'), {
-    setHeaders: (res, path, stat) => {
-      if (path.endsWith('.js')) {
-        res.setHeader('Content-Type', 'application/javascript');
-      }
-    },
-  })
-);
+// 정적 파일 서빙 설정 수정 지금 안되니까 일단 주석
+// app.use(
+//   express.static(path.join(__dirname, '../../dist'), {
+//     mimeTypes: {
+//       js: 'application/javascript',
+//     },
+//   })
+// );
 
 app.use(bodyParser.json());
 
@@ -32,6 +33,19 @@ app.get('/signup', (req, res) => {
 
 app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/adminPage.html'));
+});
+
+app.post('/signup', async (req, res) => {
+  const { id, password, name } = req.body;
+  const result = await addUser(id, password, name);
+
+  if (result.success) {
+    // 회원가입 성공 시 로그인 페이지로 이동
+    res.redirect('/login');
+  } else {
+    // 회원가입 실패 시 에러 메시지 출력
+    res.status(400).json({ error: result.error });
+  }
 });
 
 app.listen(port, () => {
