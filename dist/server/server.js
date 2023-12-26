62ee8ecb69d1ee071dee63f944a6bae43e4a07a7
ChaeYi <chaeyi990731@gmail.com>
2023-12-26 12:49:52 +0900

    :package: Package: server.ts 컴파일

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,37 +1,93 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const app = express();
-const port = 9997;
-
-// 정적 파일 서빙 설정 수정
-app.use(
-  express.static(path.join(__dirname, '../../dist'), {
-    mimeTypes: {
-      js: 'application/javascript',
-    },
-  })
-);
-
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/mainPage.html'));
+"use strict";
+// server.ts
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var path_1 = __importDefault(require("path"));
+var db_1 = require("../database/db"); // data.ts 파일에서 export한 함수들을 불러옴
+var app = (0, express_1.default)();
+var port = 9997;
+// 정적 파일 서빙 설정 수정 지금 안되니까 일단 주석
+// app.use(
+//   express.static(path.join(__dirname, '../../dist'), {
+//     mimeTypes: {
+//       js: 'application/javascript',
+//     },
+//   })
+// );
+app.use(body_parser_1.default.json());
+app.get('/', function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, '../../public/mainPage.html'));
 });
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/loginPage.html'));
+app.get('/login', function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, '../../public/loginPage.html'));
 });
-
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/signupPage.html'));
+app.get('/signup', function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, '../../public/signupPage.html'));
 });
-
-app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/adminPage.html'));
+app.get('/admin', function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, '../../public/adminPage.html'));
 });
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.post('/signup', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, id, password, name, result;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = req.body, id = _a.id, password = _a.password, name = _a.name;
+                return [4 /*yield*/, (0, db_1.addUser)(id, password, name)];
+            case 1:
+                result = _b.sent();
+                if (result.success) {
+                    // 회원가입 성공 시 로그인 페이지로 이동
+                    res.redirect('/login');
+                }
+                else {
+                    // 회원가입 실패 시 에러 메시지 출력
+                    res.status(400).json({ error: result.error });
+                }
+                return [2 /*return*/];
+        }
+    });
+}); });
+app.listen(port, function () {
+    console.log("Server is running at http://localhost:".concat(port));
 });
