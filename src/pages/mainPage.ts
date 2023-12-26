448f5438a051a0a0b3653919584de0bc1d46734d
ChaeYi <chaeyi990731@gmail.com>
2023-12-26 16:24:16 +0900

    :hammer: Modify: 버튼 이벤트 구현

---
+++ src/pages/mainPage.ts
@@ -0,0 +1,7 @@
+document.getElementById('loginBtn').addEventListener('click', () => {
+  window.location.href = '/login';
+});
+
+document.getElementById('signupBtn').addEventListener('click', () => {
+  window.location.href = '/signup';
+});
