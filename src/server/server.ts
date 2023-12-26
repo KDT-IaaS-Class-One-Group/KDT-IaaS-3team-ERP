f0a90639652dd30f808fde13c7966e483a57b029
ChaeYi <chaeyi990731@gmail.com>
2023-12-26 11:16:16 +0900

    :poop: Bad: 정적파일 오류
    html은 괜찮은데 js파일이 content-type이 html/text로 저장이됨

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,10 +5,33 @@ import path from 'path';
 const app = express();
 const port = 9997;
 
+// 정적 파일 서빙 설정 (express.static을 앞쪽으로 이동)
+app.use(
+  express.static(path.join(__dirname, '../../public'), {
+    setHeaders: (res, path, stat) => {
+      if (path.endsWith('.js')) {
+        res.setHeader('Content-Type', 'application/javascript');
+      }
+    },
+  })
+);
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/mainPage.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/loginPage.html'));
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/signupPage.html'));
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/adminPage.html'));
 });
 
 app.listen(port, () => {
