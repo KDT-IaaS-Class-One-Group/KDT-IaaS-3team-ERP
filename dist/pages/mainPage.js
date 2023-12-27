c17d34e90dd351a43898fc594e53784f02f9f728
ChaeYi <chaeyi990731@gmail.com>
2023-12-27 10:40:15 +0900

    :package: Package: 클릭이벤트 구현
    로그인페이지 회원가입 페이지 이동 클릭 이벤트 컴파일

---
+++ dist/pages/mainPage.js
@@ -0,0 +1,7 @@
+"use strict";
+document.getElementById('loginBtn').addEventListener('click', function () {
+    window.location.href = '/login';
+});
+document.getElementById('signupBtn').addEventListener('click', function () {
+    window.location.href = '/signup';
+});
