b09407790d698e29139df2d90c1ba885c831e45c
ChaeYi <chaeyi990731@gmail.com>
2023-12-27 17:46:55 +0900

    :sparkles: Feat: header 작성
    초기화 하고 헤더 만들어보기

---
+++ jjanbari/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function Headers() {
+  return (
+    <div className="Header">
+      <p>이건 헤더입니다.</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Headers />
+    </div>
+  );
+}
+
+export default App;
