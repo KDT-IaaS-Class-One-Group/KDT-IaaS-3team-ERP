57bc4ec6f5b186fe41a5121ea844875d6bdd1729
Ho Nyeong <dev.honing@gmail.com>
2023-12-28 17:34:33 +0900

    :sparkles: Feat: 콘솔에 JSON 객체를 출력하는 데에 성공

--- test/renderToHtml.js
+++ test/renderToHtml.js
@@ -2,7 +2,10 @@
 
 // HTML로 렌더링하는 함수
 function renderToHtml(data) {
-  const renderedHtml = `<div>${data}</div>`;
+  // JSON 객체의 내용을 문자열로 변환하여 표시
+  const contentToDisplay = `<p>${JSON.stringify(data, null, 2)}</p>`;
+
+  const renderedHtml = `<div>${contentToDisplay}</div>`;
   return renderedHtml;
 }
 
