1671fc9e2c41ee0cf52cf4296e6c3f6aec3b36ec
ChaeYi <chaeyi990731@gmail.com>
2023-12-28 16:01:16 +0900

    :speech_balloon: Comments: 메서드, 함수 설명 주석 처리

--- datatest/test.js
+++ datatest/test.js
@@ -26,5 +26,8 @@ console.log('전체 강의실 갯수:', totalRooms);
 const totalStudent = studentCounts.reduce((sum, room) => sum + room.studentCount, 0);
 console.log('전체 학생수:', totalStudent);
 
+//하나씩 순회하면서 콜백 함수를 실행
+//이 콜백 함수는 현재까지 가장 학생 수가 많은 강의실 정보인 maxRoom과 현재 순회 중인 강의실 room을 비교하여 더 많은 학생이 있는 경우 room으로 갱신
 const maxStudentRoom = studentCounts.reduce((maxRoom, room) => (room.studentCount > maxRoom.studentCount ? room : maxRoom), { studentCount: -1 });
+//{ studentCount: -1 }이 주어져 있습니다. 이 초기값은 학생 수가 0 이상이라는 가정하에 설정되어있다.
 console.log('가장 많은 학생이 있는 강의실:', maxStudentRoom.room, '학생 수:', maxStudentRoom.studentCount);
