e587c45f15fdad606eb3f6970d70eb865fda61a5
samuel21695 <samuel21695@gmail.com>
2023-12-28 13:09:24 +0900

    ✨ Feat: create Room component
    also set with the Interface

---
+++ jjanbari/src/Room.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NumberLiteralType } from "typescript";
+
+interface Class {
+  instructor: string;
+  field: string;
+  computers: number;
+  students: string[];
+}
+
+const Room = ({ instructor, field, computers, students }: Class) => {
+
+}
