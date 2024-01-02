0f8438dbc2bfae7088b0ac8c368814e8ece2b441
samuel21695 <samuel21695@gmail.com>
2024-01-02 12:41:37 +0900

    ✨ Feat: create onClick event
    onClick 이벤트 사용하여 페이지 이동 기능 구현

--- jjanbari/src/App.tsx
+++ jjanbari/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+function handleClick() {
+  window.location.href = '/login';
+}
 
 function App() {
   return (
     <div className="App">
       <h1>메인페이지입니다.</h1>
-      <button id="loginPageButton">Login</button>
+      <button onClick={handleClick}>Login</button>
     </div>
   );
 }
