672bf2826e1e306784c323ad7ff82645a9ba45ae
samuel21695 <samuel21695@gmail.com>
2024-01-02 11:48:18 +0900

    🧪 Test: test exporting login page to check succesfull loading other pages
    로그인 페이지 정상적으로 컴포넌트화 되었는지 확인위해 app.tsx에 import 실험 (복구 예정)

--- jjanbari/src/index.tsx
+++ jjanbari/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './components/login'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
