f171227f97082f292277f4d450ff312d6049db00
Yusmin97 <tmdamsi97@gmail.com>
2024-01-02 13:08:23 +0900

    ✨ Feat: 페이지 별 라우트
    메인 페이지와 회원가입, 로그인 페이지의 라우팅 처리

--- jjanbari/src/App.tsx
+++ jjanbari/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import SignupPage from './login-sign-up/SignupPage';
+import LoginPage from './login-sign-up/LoginPage';
+import MainPage from './main/Main';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
