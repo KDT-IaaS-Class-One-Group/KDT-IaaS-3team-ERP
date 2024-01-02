6355fe37578fa17dbddcb9ecf4fc98c7ab7bfb88
ChaeYi <chaeyi990731@gmail.com>
2024-01-02 15:18:24 +0900

    :sparkles: Feat: singupPage route 매핑하기

--- jjanbari/src/App.tsx
+++ jjanbari/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './components/mainPage';
 import LoginPage from './components/loginPage';
+import SignupPage from './components/signupPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<MainPage />} />
       </Routes>
