d9debbccd41c83d3fee6af864fb4815ac7576252
Ho Nyeong <dev.honing@gmail.com>
2024-01-02 13:38:47 +0900

    :poop: Bad: 오류 메시지는 해결했지만, 컴포넌트가 렌더링되지 않음

--- jjanbari/src/App.tsx
+++ jjanbari/src/App.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Component/Header/Header";
-import Container from "./Component/Container/Container";
+import Container from './Component/Container/Container';
 import Footer from "./Component/Footer/Footer";
 
 const App: React.FC = () => {
@@ -11,11 +11,12 @@ const App: React.FC = () => {
     <Router>
       <div className="App">
         <Header />
-        <Route path="/" exact component={Container} />
-        <Route path="/admin" component={/* Admin 페이지 컴포넌트 */} />
-        <Route path="/login" component={/* 로그인 페이지 컴포넌트 */} />
-        <Route path="/signup" component={/* 회원 가입 페이지 컴포넌트 */} />
-        <Route path="/withdraw" component={/* 회원 탈퇴 페이지 컴포넌트 */} />
+        <Container />
+        <Route path="/" />
+        <Route path="/admin" />
+        <Route path="/login" />
+        <Route path="/signup" />
+        <Route path="/withdraw" />
         <Footer />
       </div>
     </Router>
