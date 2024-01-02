0afafeb9aa6dc501e526467f9d1a0adcd2ad09b2
ChaeYi <chaeyi990731@gmail.com>
2024-01-02 13:01:38 +0900

    :poop: Bad: get요청으로 signupPage 받아오려했는데 받아와지지 않음

--- jjanbari/server.js
+++ jjanbari/server.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.post('/login', async (req, res) => {
@@ -27,6 +27,27 @@ app.post('/login', async (req, res) => {
     }
   } catch (error) {
     console.error('Error during login:', error.message);
+    res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
+  }
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'));
+});
+
+app.post('/signup', async (req, res) => {
+  const { id, password, name } = req.body;
+
+  try {
+    const users = await query('SELECT * FROM userInfo WHERE id = ?', [id]);
+    if (users.length > 0) {
+      res.status(409).json({ success: false, error: '이미 등록된 ID입니다' });
+    } else {
+      await query('INSERT INTO userInfo (id, password, name) VALUES (?, ?, ?)', [id, password, name]);
+      res.json({ success: true });
+    }
+  } catch (error) {
+    console.error('Error during signup:', error.message);
     res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
   }
 });
