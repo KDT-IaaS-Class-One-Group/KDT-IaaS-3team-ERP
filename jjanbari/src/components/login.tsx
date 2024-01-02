3a04f6e9714a14d0158953ae2a930044e95e4c65
samuel21695 <samuel21695@gmail.com>
2024-01-02 11:44:09 +0900

    ✨ Feat: Login page componentize
    로그인 페이지 컴포넌트화

--- jjanbari/src/components/login.tsx
+++ jjanbari/src/components/login.tsx
@@ -1,3 +1,19 @@
-function Login() {}
+function Login() {
+  return (
+    <div className="Login">
+      <h2>로그인 하세요</h2>
+      <form action="/login" method="post">
+        <input type="text" name="name" placeholder="이름"></input>
+        <input type="text" name="id" placeholder="아이디"></input>
+        <input type="password" name="password" placeholder="비밀번호"></input>
+        <input type="submit" value="로그인"></input>
+      </form>
+      <form action="/admin" method="post">
+        <input type="submit" value="관리자 페이지 로그인"></input>
+      </form>
+      <button id="signUpPageButton">회원가입</button>
+    </div>
+  )
+} 
 
 export default Login;
