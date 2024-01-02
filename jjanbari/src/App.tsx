c5cd2b8d65df28c6e9f544172df6f7e3693d47af
samuel21695 <samuel21695@gmail.com>
2024-01-02 14:53:41 +0900

    ✨ Feat: use useState to implement page movement logic
    useState 메서드 사용하요 페이지 이동 로직 구현

--- jjanbari/src/App.tsx
+++ jjanbari/src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link} from "react-router-dom"
+import React, {useState} from 'react';
+import Login from './components/login';
+
 import './App.css';
 
-function handleClick() {
-  window.location.href = '/login';
-}
-
 function App() {
+  const [page, setPage] = useState('main');
+
+  function handleClick() {
+    setPage('login');
+  }
+
+  if (page === 'login') {
+    return <Login />;
+  }
+
   return (
     <div className="App">
       <h1>메인페이지입니다.</h1>
