329887b9ec4991ba2704c4295f916002034368dc
samuel21695 <samuel21695@gmail.com>
2024-01-03 12:47:54 +0900

    ✨ Feat: loading router file
    but no contents inerfile
    라우터 파일 불러오기. 파일 내용은 X

--- jjanbari/src/Router.tsx
+++ jjanbari/src/Router.tsx
@@ -4,3 +4,18 @@ import Admin from "./components/Admin";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Product from "./components/Product";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<Admin />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/Product" element={<Product />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
--- jjanbari/src/index.tsx
+++ jjanbari/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './components/Login'
+import Router from './Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
