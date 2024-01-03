8797e9524e456f33462e569639ed19b415fe6b33
samuel21695 <samuel21695@gmail.com>
2024-01-03 12:28:46 +0900

    ✏️ Rename: rename files
    파일 이름 변경

--- jjanbari/src/App.tsx
+++ jjanbari/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import Login from './components/login';
+import Login from './components/Login'
 
 import './App.css';
 
--- jjanbari/src/index.tsx
+++ jjanbari/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './components/login'
+import Login from './components/Login'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
