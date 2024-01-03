37798b602df0f7b32b87042763e77a5dc6f23f8f
samuel21695 <samuel21695@gmail.com>
2024-01-03 13:08:36 +0900

    ✨ Feat: signup page route setting

--- jjanbari/src/components/Signup.tsx
+++ jjanbari/src/components/Signup.tsx
@@ -1,8 +1,9 @@
 function Signup() {
+
   return (
   <div>
     <h2>회원가입 하세요</h2>
-      <form action="/signup" method="post">
+      <form action="/login">
       <input type="text" name="name" placeholder="이름"></input>
       <input type="text" name="id" placeholder="아이디"></input>
       <input type="password" name="password" placeholder="비밀번호"></input>
