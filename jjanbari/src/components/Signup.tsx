29c706087716c9f0f878d049601b95bdb7965633
samuel21695 <samuel21695@gmail.com>
2024-01-03 12:32:16 +0900

    📝 feat:  Signup page updated

---
+++ jjanbari/src/components/Signup.tsx
@@ -0,0 +1,15 @@
+function Admin() {
+  return (
+  <div>
+    <h2>회원가입 하세요</h2>
+      <form action="/signup" method="post">
+      <input type="text" name="name" placeholder="이름"></input>
+      <input type="text" name="id" placeholder="아이디"></input>
+      <input type="password" name="password" placeholder="비밀번호"></input>
+      <input type="submit" value="회원가입"></input>
+  </form>
+  </div>
+  )
+}
+
+export default Admin;
