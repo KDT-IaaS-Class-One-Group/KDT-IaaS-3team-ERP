c366e6dd1ff5c9e055ddcf6db2c80d4a41ecb8c1
Yusmin97 <tmdamsi97@gmail.com>
2024-01-03 13:05:02 +0900

    🔨 Modify: post 요청 변경
    axios에서 async & await 으로 변경하여 작성(axios 의 핸들링 연습이 더 필요 할 것 같다)

--- jjanbari/src/login-sign-up/LoginPage.tsx
+++ jjanbari/src/login-sign-up/LoginPage.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import LoginForm from './LoginForm';
-import axios from 'axios';
 
 const LoginPage: React.FC = () => {
-  const handleLogin = (userId: string, password: string) => {
-    axios
-      .post('/login-in', { userId, password }, { headers: { 'Content-Type': 'application/json' } })
-      .then((response) => {
-        if (response.status === 200) {
-          alert('Login successful');
-          window.location.href = '/';
-        } else {
-          alert('Invalid credentials');
-        }
-      })
-      .catch((error) => {
-        console.error('Error during login:', error);
+  const handleLogin = async (userId: string, password: string) => {
+    try {
+      const response = await fetch('http://localhost:5000/login-in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId, password }),
       });
+
+      if (response.ok) {
+        alert('Login successful');
+        window.location.href = '/';
+      } else {
+        alert('Invalid credentials');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   };
 
   return (
--- jjanbari/src/login-sign-up/SignupPage.tsx
+++ jjanbari/src/login-sign-up/SignupPage.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import SignupForm from './SignupForm';
-import axios from 'axios';
 
 function SignupPage() {
-  const handleSignup = (userId: string, password: string, userName: string) => {
-    axios
-      .post('/signUp/save', { userId, password, userName }, { headers: { 'Content-Type': 'application/json' } })
-      .then((response) => {
-        if (response.status === 200) {
-          alert('Signup successful');
-          window.location.href = '/login';
-        } else {
-          alert('Error during signup');
-        }
-      })
-      .catch((error) => {
-        console.error('Error during signup:', error);
+  const handleSignup = async (userId: string, password: string, userName: string) => {
+    try {
+      const response = await fetch('http://localhost:5000/signUp/save', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId, password, userName }),
       });
+
+      if (response.ok) {
+        alert('Signup successful');
+        window.location.href = '/login';
+      } else {
+        alert('Error during signup');
+      }
+    } catch (error) {
+      console.error('Error during signup:', error);
+    }
   };
 
   return (
