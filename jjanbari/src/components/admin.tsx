fc39417801ec7d3a3c18c33ba5047bde7fc8da8f
samuel21695 <samuel21695@gmail.com>
2024-01-03 14:19:22 +0900

    ✨ Feat: admin page update
    admin 페이지 업데이트

--- jjanbari/src/components/admin.tsx
+++ jjanbari/src/components/admin.tsx
@@ -1,7 +1,13 @@
 function Admin() {
+
+  function productClick() {
+    window.location.href="/product"
+  }
+
   return (
   <div>
-    <button id="productUploadPageButton">상품 등록</button>
+    <h2>관리자용 페이지입니다.</h2>
+    <button id="productUploadPageButton" onClick={productClick}>상품 등록</button>
   </div>
   )
 }
