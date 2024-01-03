c57fa4e0368b8899fa8a798cbcfbdd072483b945
samuel21695 <samuel21695@gmail.com>
2024-01-03 12:43:53 +0900

    ✨ import pages
    컴포넌트 파일들 import로 불러오기

--- jjanbari/src/Router.tsx
+++ jjanbari/src/Router.tsx
@@ -1,1 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Admin from "./components/Admin";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Product from "./components/Product";
