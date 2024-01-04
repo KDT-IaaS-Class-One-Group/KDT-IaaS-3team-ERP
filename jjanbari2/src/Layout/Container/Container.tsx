ecef7a5d28c1505bab156523335cf794974241a8
Ho Nyeong <dev.honing@gmail.com>
2024-01-05 00:57:40 +0900

    :sparkles: Feat: 회원 가입 페이지를 컴포넌트로 정의하고 적용
    - SignupPage 컴포넌트를 Container 컴포넌트의 path에 element로 추가

--- jjanbari2/src/Layout/Container/Container.tsx
+++ jjanbari2/src/Layout/Container/Container.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AdminPage from "../../Pages/Admin/AdminPage";
 import LoginPage from "../../Pages/Login/LoginPage";
+import SignupPage from "../../Pages/Signup/SignupPage";
 
 import './Container.css';
 
@@ -13,7 +14,7 @@ const Container = () => {
       <Routes>
         <Route path="/admin" element={<AdminPage/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
-        <Route path="/signup"></Route>
+        <Route path="/signup" element={<SignupPage/>}></Route>
         <Route path="/withdraw"></Route>
         <Route path="/"></Route>
       </Routes>
