c877d88de6b276479821d0d089b9b6da849f47f1
Ho Nyeong <dev.honing@gmail.com>
2024-01-04 23:59:12 +0900

    :sparkles: Feat: Header 컴포넌트를 정의하고 App.tsx에 적용

--- jjanbari2/src/App.tsx
+++ jjanbari2/src/App.tsx
@@ -1,11 +1,13 @@
 // src/App.tsx
 import React from "react";
+import Header from "./Layout/Header/Header";
 
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 };
