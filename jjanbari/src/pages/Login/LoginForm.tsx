02b02c6d8bdc2ac6621142c2f353684408ba61c3
Ho Nyeong <dev.honing@gmail.com>
2024-01-04 23:16:11 +0900

    :bug: Fix: 폼 작성이 되지 않는 오류는 해결했지만 모듈 import에 어려움을 겪고 있음

--- jjanbari/src/pages/Login/LoginForm.tsx
+++ jjanbari/src/pages/Login/LoginForm.tsx
@@ -1,7 +1,6 @@
 // src/pages/Login/LoginForm.tsx
 
 import React, { useState, ChangeEvent, FormEvent } from "react";
-import { useNavigate } from "react-router-dom";
 
 interface LoginFormData {
   id: string;
@@ -9,7 +8,6 @@ interface LoginFormData {
 }
 
 const LoginForm: React.FC = () => {
-  const navigate = useNavigate();
 
   const [loginFormData, setLoginFormData] = useState<LoginFormData>({
     id: "",
@@ -48,11 +46,11 @@ const LoginForm: React.FC = () => {
         if (data.role === 'admin') {
           // 관리자 로그인 성공
           console.log("관리자로 로그인하였습니다.");
-          navigate("/admin"); // 관리자 페이지로 이동
+          
         } else if (data.role === 'user') {
           // 사용자 로그인 성공
           console.log("사용자로 로그인하였습니다.");
-          navigate("/main"); // 메인 페이지로 이동
+          
         }
       } else {
         console.error("로그인 실패:", response.statusText);
@@ -71,7 +69,7 @@ const LoginForm: React.FC = () => {
           아이디:
           <input
             type="text"
-            name="userID"
+            name="id"
             value={loginFormData.id}
             onChange={handleInputChange}
             required
@@ -82,7 +80,7 @@ const LoginForm: React.FC = () => {
           비밀번호:
           <input
             type="password"
-            name="userPW"
+            name="password"
             value={loginFormData.password}
             onChange={handleInputChange}
             required
