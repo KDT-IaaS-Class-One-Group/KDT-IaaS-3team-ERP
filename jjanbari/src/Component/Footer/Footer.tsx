31dbdafd6cbfa365bd069e4434543b9571d029c3
Ho Nyeong <dev.honing@gmail.com>
2024-01-04 12:23:17 +0900

    :truck: Move: 병합을 위한 디렉토리 이동

--- jjanbari/src/Component/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-// src/Component/Footer/Footer.tsx
-
-import "./Footer.css";
-
-const Footer = () => {
-  return <div id="footer">© 2023 KDT IaaS Team 3. All Rights Reserved.</div>;
-};
-
-export default Footer;
