4ae72fe408cd82336d6c10f926c0c842702abd59
Yusmin97 <tmdamsi97@gmail.com>
2024-01-04 12:43:57 +0900

    🔥 Delete: 바닐라 방식 파일 삭제

--- test/app.js
+++
@@ -1,5 +0,0 @@
-// test/app.js
-
-// 함수 모듈 가져오기
-const readJsonFile = require("./readJsonFile");
-const printJsonFile = require("./printJsonFile");
--- test/readJsonFile.js
+++
@@ -1,8 +0,0 @@
-// test/readJsonFile.js
-
-// JSON 파일을 읽어오는 함수
-function readJsonFile() {
-
-}
-
-module.exports = readJsonFile;
