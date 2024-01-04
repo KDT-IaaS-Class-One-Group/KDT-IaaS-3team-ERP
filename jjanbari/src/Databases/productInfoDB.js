6be22e2291e81751dc5f5b70d01522b67a9ab44b
Ho Nyeong <dev.honing@gmail.com>
2024-01-04 21:57:02 +0900

    :bug: Fix: DB 연결 비밀번호 설정 변경

--- jjanbari/src/Databases/productInfoDB.js
+++ jjanbari/src/Databases/productInfoDB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'qwer123',
+  password: '1234',
   database: 'productInfo',
 });
 
--- jjanbari/src/Databases/userInfoDB.js
+++ jjanbari/src/Databases/userInfoDB.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'qwer123',
+  password: '1234',
   database: 'userInfo',
 });
 
