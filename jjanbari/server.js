7133fdb7fba5edef27a75d79b1eba39657bf3f91
Ho Nyeong <dev.honing@gmail.com>
2024-01-05 10:36:13 +0900

    :hammer: Modify: 확인할 회원 가입 로직 이외의 다른 코드를 비활성화
    - 주석 처리를 통해 비활성화

--- jjanbari/server.js
+++ jjanbari/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const userInfo = require('./src/Databases/userInfo');
-const {query} = require('./src/Databases/productInfoDB');
+// const {query} = require('./src/Databases/productInfoDB');
 
 const app = express();
 const port = 3001;
@@ -14,121 +14,91 @@ app.use(bodyParser.json());
 
 // 회원 가입 라우트
 app.post('/signup', async (req, res) => {
-  const { id, password, name } = req.body;
-
-  try {
-    // 아이디로 사용자 조회
-    const existingUser = await userInfo.getUserById(id);
-
-    if (existingUser) {
-      console.error('회원 가입 실패: 이미 존재하는 아이디입니다.');
-      res.status(409).send('이미 존재하는 아이디입니다.');
-    } else {
-      // 사용자 정보 저장
-      const insertDataQuery = `
-        INSERT INTO userInfo (id, password, name)
-        VALUES (?, ?, ?);
-      `;
-
-      userInfo.connection.query(insertDataQuery, [id, password, name], (error, results) => {
-        if (error) {
-          console.error('회원 가입 실패:', error);
-          res.status(500).send('회원 가입에 실패했습니다. 다시 시도해주세요.');
-        } else {
-          console.log('회원 가입 정보 저장 성공:', req.body);
-          res.sendStatus(200);
-        }
-      });
-    }
-  } catch (error) {
-    console.error('회원 가입 실패:', error);
-    res.status(500).send('회원 가입에 실패했습니다. 다시 시도해주세요.');
-  }
 });
 
-// 로그인 라우트
-app.post('/login', async (req, res) => {
-  const { userID, userPW } = req.body;
+// // 로그인 라우트
+// app.post('/login', async (req, res) => {
+//   const { userID, userPW } = req.body;
 
-  try {
-    // 아이디로 사용자 조회
-    const user = await userInfo.getUserById(userID);
+//   try {
+//     // 아이디로 사용자 조회
+//     const user = await userInfo.getUserById(userID);
 
-    if (user) {
-      // 비밀번호 비교
-      if (user.userPW === userPW) {
-        if (user.id === 'adroot') {
-          // 관리자 로그인 성공
-          console.log('관리자로 로그인하였습니다.');
-          res.status(201).json({ role: 'admin' });
-        } else {
-          // 사용자 로그인 성공
-          console.log('사용자로 로그인하였습니다.');
-          res.status(200).json({ role: 'user' });
-        }
-      } else {
-        console.error('로그인 실패: 비밀번호가 일치하지 않습니다.');
-        res.status(401).send('비밀번호가 일치하지 않습니다.');
-      }
-    } else {
-      console.error('로그인 실패: 해당 ID가 존재하지 않습니다.');
-      res.status(401).send('해당 ID가 존재하지 않습니다.');
-    }
-  } catch (error) {
-    console.error('로그인 실패:', error);
-    res.status(500).send('로그인에 실패했습니다. 다시 시도해주세요.');
-  }
-});
+//     if (user) {
+//       // 비밀번호 비교
+//       if (user.userPW === userPW) {
+//         if (user.id === 'adroot') {
+//           // 관리자 로그인 성공
+//           console.log('관리자로 로그인하였습니다.');
+//           res.status(201).json({ role: 'admin' });
+//         } else {
+//           // 사용자 로그인 성공
+//           console.log('사용자로 로그인하였습니다.');
+//           res.status(200).json({ role: 'user' });
+//         }
+//       } else {
+//         console.error('로그인 실패: 비밀번호가 일치하지 않습니다.');
+//         res.status(401).send('비밀번호가 일치하지 않습니다.');
+//       }
+//     } else {
+//       console.error('로그인 실패: 해당 ID가 존재하지 않습니다.');
+//       res.status(401).send('해당 ID가 존재하지 않습니다.');
+//     }
+//   } catch (error) {
+//     console.error('로그인 실패:', error);
+//     res.status(500).send('로그인에 실패했습니다. 다시 시도해주세요.');
+//   }
+// });
 
-// 관리자 페이지 라우트
-app.get('/admin', (req, res) => {
-  res.send('관리자 페이지입니다.');
-});
+// // 관리자 페이지 라우트
+// app.get('/admin', (req, res) => {
+//   res.send('관리자 페이지입니다.');
+// });
 
-app.post('/addProducts', async (req, res) => {
-  const { name, price, quantity } = req.body;
+// app.post('/addProducts', async (req, res) => {
+//   const { name, price, quantity } = req.body;
 
-  try {
-    // 먼저 동일한 name과 price를 가진 상품이 있는지 확인합니다.
-    const products = await query('SELECT * FROM products WHERE name = ? AND price = ?', [name, price]);
+//   try {
+//     // 먼저 동일한 name과 price를 가진 상품이 있는지 확인합니다.
+//     const products = await query('SELECT * FROM products WHERE name = ? AND price = ?', [name, price]);
 
-    if (products.length > 0) {
-      // 동일한 name과 price를 가진 상품이 이미 있으면, 해당 상품의 quantity를 업데이트합니다.
-      await query('UPDATE products SET quantity = quantity + ? WHERE name = ? AND price = ?', [quantity, name, price]);
-    } else {
-      // 동일한 name과 price를 가진 상품이 없으면, 새로운 상품을 추가합니다.
-      await query('INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)', [name, price, quantity]);
-    }
+//     if (products.length > 0) {
+//       // 동일한 name과 price를 가진 상품이 이미 있으면, 해당 상품의 quantity를 업데이트합니다.
+//       await query('UPDATE products SET quantity = quantity + ? WHERE name = ? AND price = ?', [quantity, name, price]);
+//     } else {
+//       // 동일한 name과 price를 가진 상품이 없으면, 새로운 상품을 추가합니다.
+//       await query('INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)', [name, price, quantity]);
+//     }
 
-    res.json({ success: true });
-  } catch (error) {
-    console.error('Error during product registration:', error.message);
-    res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
-  }
-});
+//     res.json({ success: true });
+//   } catch (error) {
+//     console.error('Error during product registration:', error.message);
+//     res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
+//   }
+// });
 
-app.get('/products', async (req, res) => {
-  try {
-    const products = await query('SELECT * FROM products');
-    res.json(products);
-  } catch (error) {
-    console.error('Error during fetching products:', error.message);
-    res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
-  }
-});
+// app.get('/products', async (req, res) => {
+//   try {
+//     const products = await query('SELECT * FROM products');
+//     res.json(products);
+//   } catch (error) {
+//     console.error('Error during fetching products:', error.message);
+//     res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
+//   }
+// });
 
-app.put('/products/:name', async (req, res) => {
-  const { name } = req.params;
-  const { quantity } = req.body;
+// app.put('/products/:name', async (req, res) => {
+//   const { name } = req.params;
+//   const { quantity } = req.body;
 
-  try {
-    await query('UPDATE products SET quantity = quantity - ? WHERE name = ?', [quantity, name]);
-    res.json({ success: true });
-  } catch (error) {
-    console.error('Error during updating product:', error.message);
-    res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
-  }
-});
+//   try {
+//     await query('UPDATE products SET quantity = quantity - ? WHERE name = ?', [quantity, name]);
+//     res.json({ success: true });
+//   } catch (error) {
+//     console.error('Error during updating product:', error.message);
+//     res.status(500).json({ success: false, error: '서버 오류가 발생했습니다.' });
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`서버 ON: http://localhost:${port}`);
