bcfae8d63357a1e2f74369f02c9e78a1561ef024
Ho Nyeong <dev.honing@gmail.com>
2024-01-05 10:49:14 +0900

    :bug: Fix: 테이블 생성 쿼리문의 양식 수정

--- jjanbari/src/Databases/userInfo.js
+++ jjanbari/src/Databases/userInfo.js
@@ -16,9 +16,9 @@ const createDatabaseQuery = 'CREATE DATABASE IF NOT EXISTS userInfo';
 const createTableQuery = `
   CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL
+  userID VARCHAR(255) NOT NULL,
+  userPW VARCHAR(255) NOT NULL,
+  userNAME VARCHAR(255) NOT NULL
   );
 `;
 
