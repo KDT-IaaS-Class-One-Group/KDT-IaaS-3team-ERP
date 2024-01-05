fc6b2f79091e4a0407157210ac9acc15e30a04e4
Ho Nyeong <dev.honing@gmail.com>
2024-01-05 10:22:40 +0900

    :sparkles: Feat: 테이블 생성 쿼리문 추가
    - userInfo DB와 users TABLE 생성

--- jjanbari/src/Databases/userInfo.js
+++ jjanbari/src/Databases/userInfo.js
@@ -1,4 +1,4 @@
-// Databases/userInfoDB.js
+// src/Databases/userInfo.js
 
 const mysql = require('mysql2');
 
@@ -6,45 +6,47 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '1234',
+  database: 'userInfo'
 })
 
 // 데이터베이스를 생성하는 쿼리문
 const createDatabaseQuery = 'CREATE DATABASE IF NOT EXISTS userInfo';
 
-// MariaDB 서버에 연결하면, userInfo DB 생성
+// 테이블을 생성하는 쿼리문
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
+  );
+`;
+
+// MariaDB 서버에 연결하면, userInfo DB 생성 및 users TABLE 생성
 connection.connect((error) => {
   if (error) {
-    console.error('데이터베이스 연결 오류: ', error.message);
+    console.error('userInfo DB 연결 실패: ', error.message);
   } else {
-    console.log('데이터베이스에 연결되었습니다.');
+    console.log('userInfo DB 연결 성공');
 
+    // 데이터베이스 생성 쿼리 실행
     connection.query(createDatabaseQuery, (dbError) => {
       if (dbError) {
-        console.error('데이터베이스 생성 오류: ', dbError.message);
+        console.error('userInfo DB 생성 실패: ', dbError.message);
       } else {
-        console.log('데이터베이스 생성 성공 또는 이미 존재함');
+        console.log('userInfo DB 생성 성공 또는 이미 존재');
+
+        // 테이블 생성 쿼리 실행
+        connection.query(createTableQuery, (tableError) => {
+          if (tableError) {
+            console.error('users TABLE 생성 실패: ', tableError.message);
+          } else {
+            console.log('users TABLE 생성 성공 또는 이미 존재');
+          }
+        });
       }
     });
   }
 });
-// // 아이디로 사용자 조회
-// const getUserById = (id) => {
-//   return new Promise((resolve, reject) => {
-//     const selectDataQuery = `
-//       SELECT * FROM userInfo
-//       WHERE id = ?;
-//     `;
-//     connection.query(selectDataQuery, [id], (error, results) => {
-//       if (error) {
-//         reject(error);
-//       } else {
-//         resolve(results[0]);
-//       }
-//     });
-//   });
-// };
 
-module.exports = {
-  connection
-  // getUserById,
-};
+module.exports = connection;
