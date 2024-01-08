bb6dadb86de0b8f2967cb711e9315f88dadd0033
samuel21695 <samuel21695@gmail.com>
2024-01-08 16:54:41 +0900

    ✨ Feat: puklling out Header Component

--- jjanbari/src/App.tsx
+++ jjanbari/src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 import React from "react";
 
-import Header from "./Layout/Header/Header";
 import Footer from "./Layout/Footer/Footer";
 import Router from "./Layout/Routes/Router";
 import "./App.css";
@@ -9,7 +8,6 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <Router />
       <Footer />
     </div>
