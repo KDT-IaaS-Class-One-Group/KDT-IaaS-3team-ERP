8b3c8db2b926b0def0915decd7a848eb0fd2d53a
ChaeYi <chaeyi990731@gmail.com>
2024-01-08 17:18:00 +0900

    :pencil2: Rename: 대문자 소문자로 변경

--- jjanbari/src/Layout/Header/Header.tsx
+++ jjanbari/src/Layout/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
       <a href="/login">로그인 페이지로</a>
       <a href="signup">회원 가입 페이지로</a>
       <a href="withdraw">회원 탈퇴 페이지로</a>
-      <a href="Mypage">마이페이지</a>
+      <a href="mypage">마이페이지</a>
       <a href="/">메인 페이지로</a>
     </div>
   );
