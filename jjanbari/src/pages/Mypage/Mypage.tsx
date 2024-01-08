a60f342eabacc9ec5f4e57de26c7a18a640ee97e
ChaeYi <chaeyi990731@gmail.com>
2024-01-08 17:41:07 +0900

    :sparkles: Feat: 간단히 페이지 생성

--- jjanbari/src/pages/Mypage/Mypage.tsx
+++ jjanbari/src/pages/Mypage/Mypage.tsx
@@ -1,7 +1,10 @@
+import WithdrawPage from "./Withdraw/WithdrawPage";
+
 const Mypage = () => {
   return (
     <div id="container">
       <h1>마이페이지</h1>
+      < WithdrawPage />
     </div>
   );
 };
