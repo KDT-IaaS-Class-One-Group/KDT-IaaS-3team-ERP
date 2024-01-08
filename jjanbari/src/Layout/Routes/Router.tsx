8b054a91926596779de98663e57ae9698b1ec9be
ChaeYi <chaeyi990731@gmail.com>
2024-01-08 20:49:24 +0900

    :sparkles: Feat: 각 페이지 라우트 처리
    근데 코드가  무척 매우 더러워진 형태

--- jjanbari/src/Layout/Routes/Router.tsx
+++ jjanbari/src/Layout/Routes/Router.tsx
@@ -10,6 +10,11 @@ import MainPage from '../../pages/Main/MainPage';
 
 import './Router.css';
 import Mypage from '../../pages/Mypage/Mypage';
+import OrderPage from '../../pages/Mypage/Order/Order';
+import CartPage from '../../pages/Mypage/Cart/Cart';
+import DeliveryPage from '../../pages/Mypage/Delivery/Delivery';
+import LikePage from '../../pages/Mypage/Like/Like';
+import RecentPage from '../../pages/Mypage/Recent/Recent';
 
 const Router = () => {
   return (
@@ -20,6 +25,12 @@ const Router = () => {
         <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/withdraw" element={<WithdrawPage />}></Route>
         <Route path="/mypage" element={<Mypage />}></Route>
+        <Route path="/order" element={<OrderPage />}></Route>
+        <Route path="/cart" element={<CartPage />}></Route>
+        <Route path="/delivery" element={<DeliveryPage />}></Route>
+        <Route path="/like" element={<LikePage />}></Route>
+        <Route path="/recent" element={<RecentPage />}></Route>
+        <Route path="/withdraw" element={<WithdrawPage />}></Route>
         <Route path="/" element={<MainPage />}></Route>
       </Routes>
     </BrowserRouter>
