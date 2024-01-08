ed5f0b00b70ea7037904ea5d5107c4177f4d6ae6
samuel21695 <samuel21695@gmail.com>
2024-01-08 21:50:07 +0900

    🔥 Delete: delete unnecessary codes
    기존 admin과 main페이지에 붙여놓았던 중복되는 AdminHeader와 userHeader 삭제

--- jjanbari/src/pages/Admin/AdminPage.tsx
+++ jjanbari/src/pages/Admin/AdminPage.tsx
@@ -2,19 +2,15 @@
 
 import React from "react";
 import ProductForm from "./ProductForm";
-import AdminHeader from "../../Layout/Header/Admin/AdminHeader";
 
 
 import './AdminPage.css';
 
 const AdminPage = () => {
   return (
-    <div id="adminContainer">
-      <AdminHeader />
-      <div id="container">
-        <h1>관리자 페이지</h1>
-        <ProductForm />
-      </div>
+    <div id="container">
+      <h1>관리자 페이지</h1>
+      <ProductForm />
     </div>
   );
 };
--- jjanbari/src/pages/Main/MainPage.tsx
+++ jjanbari/src/pages/Main/MainPage.tsx
@@ -1,18 +1,14 @@
 // src/pages/Main/MainPage.tsx
 
 import ProductRender from './ProductRender';
-import Header from '../../Layout/Header/User/UserHeader';
 
 import './MainPage.css';
 
 const MainPage = () => {
   return (
-    <div id="userContainer">
-      <Header />
-      <div id="container">
-        <h1>메인 페이지</h1>
-        <ProductRender />
-      </div>
+    <div id="container">
+      <h1>메인 페이지</h1>
+      <ProductRender />
     </div>
   );
 };
