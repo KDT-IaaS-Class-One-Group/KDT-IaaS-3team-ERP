a571a985021ca31c986ce95e672f7a4c928852af
ChaeYi <chaeyi990731@gmail.com>
2024-01-08 20:57:12 +0900

    :sparkles: Feat: link to로 mypage 구성

--- jjanbari/src/pages/Mypage/Mypage.tsx
+++ jjanbari/src/pages/Mypage/Mypage.tsx
@@ -7,12 +7,21 @@ const Mypage = () => {
       <Link to="/order">
         <h1>주문목록</h1>
       </Link>
-      <h1>주문목록</h1>
-      <h1>찜한 목록</h1>
-      <h1>배송조회</h1>
-      <h1>장바구니</h1>
-      <h1>최근 본 상품</h1>
-      <h1>회원 탈퇴 페이지</h1>
+      <Link to="/like">
+        <h1>찜한목록</h1>
+      </Link>
+      <Link to="/delivery">
+        <h1>배송조회</h1>
+      </Link>
+      <Link to="/cart">
+        <h1>장바구니</h1>
+      </Link>
+      <Link to="/Recent">
+        <h1>최근 본 상품</h1>
+      </Link>
+      <Link to="/withdraw">
+        <h1>회원 탈퇴 페이지</h1>
+      </Link>
     </div>
   );
 };
