52b5f2a378da03d1054929bbcef17e803ac06a90
ChaeYi <chaeyi990731@gmail.com>
2024-01-08 17:17:27 +0900

    :sparkles: Feat: router mypage 추가

--- jjanbari/src/Layout/Routes/Router.tsx
+++ jjanbari/src/Layout/Routes/Router.tsx
@@ -1,14 +1,15 @@
 // src/Component/Routes/Router.tsx
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import AdminPage from "../../pages/Admin/AdminPage";
-import LoginPage from "../../pages/Login/LoginPage";
-import SignupPage from "../../pages/Signup/SignupPage";
-import WithdrawPage from "../../pages/Withdraw/WithdrawPage";
-import MainPage from "../../pages/Main/MainPage";
+import AdminPage from '../../pages/Admin/AdminPage';
+import LoginPage from '../../pages/Login/LoginPage';
+import SignupPage from '../../pages/Signup/SignupPage';
+import WithdrawPage from '../../pages/Withdraw/WithdrawPage';
+import MainPage from '../../pages/Main/MainPage';
 
-import "./Router.css";
+import './Router.css';
+import Mypage from '../../pages/Mypage/Mypage';
 
 const Router = () => {
   return (
@@ -18,6 +19,7 @@ const Router = () => {
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/withdraw" element={<WithdrawPage />}></Route>
+        <Route path="/mypage" element={<Mypage />}></Route>
         <Route path="/" element={<MainPage />}></Route>
       </Routes>
     </BrowserRouter>
