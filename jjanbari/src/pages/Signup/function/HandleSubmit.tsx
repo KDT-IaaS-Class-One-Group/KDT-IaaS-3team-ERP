fcaa2943125a0f936a486f3a9193118c18089e5d
ChaeYi <chaeyi990731@gmail.com>
2024-01-08 15:26:08 +0900

    :hammer: Modify: j기능적 오류 수정
    body 부분을 직접 입력 또한 IF 문을 response.ok 로 작성 하여 오류 해결

--- jjanbari/src/pages/Signup/function/HandleSubmit.tsx
+++ jjanbari/src/pages/Signup/function/HandleSubmit.tsx
@@ -1,13 +1,9 @@
-// src/handleSubmit.tsx
-
 const handleSubmit = (formData: any, navigate: any) => async (event: React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
-
   if (!formData.userID || !formData.userPW || !formData.userNAME) {
     alert('모든 필수 항목을 입력해주세요.');
     return;
   }
-
   try {
     // 서버로 회원 가입 정보 전송
     const response = await fetch('http://localhost:3001/signup', {
@@ -15,21 +11,22 @@ const handleSubmit = (formData: any, navigate: any) => async (event: React.FormE
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(formData),
+      body: JSON.stringify({
+        userID: formData.userID,
+        userPW: formData.userPW,
+        userNAME: formData.userNAME,
+      }),
     });
 
-    const result = await response.json();
-
-    if (result.success) {
+    if (response.ok) {
       alert('회원 가입이 완료되었습니다');
       navigate('/');
     } else {
-      alert(result.error || '회원 가입에 실패하였습니다');
+      alert('회원 가입에 실패하였습니다');
     }
   } catch (error: any) {
     console.error('회원 가입 중 오류 발생:', error.message);
     alert('회원 가입에 실패하였습니다');
   }
 };
-
 export default handleSubmit;
