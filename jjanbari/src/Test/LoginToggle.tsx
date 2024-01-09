26af701ed9a3bb52d36b7a228242edc77b2b310a
Ho Nyeong <dev.honing@gmail.com>
2024-01-09 14:46:05 +0900

    :bug: Fix: 새로고침해도 로그아웃되지 않도록 개선
    - useEffect Hook을 사용해 isLoggedIn이 변경될 때마다 해당 값을 sessionStorage에 저장

--- jjanbari/src/Test/LoginToggle.tsx
+++ jjanbari/src/Test/LoginToggle.tsx
@@ -1,10 +1,19 @@
 // src/Test/LoginToggle.tsx
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from './Login';
 
 const LoginToggle: React.FC = () => {
-  const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+  const [isLoggedIn, setLoggedIn] = useState<boolean>(() => {
+    // 초기 상태를 sessionStorage에서 가져오기
+    const storedLoggedIn = sessionStorage.getItem('isLoggedIn');
+    return storedLoggedIn ? JSON.parse(storedLoggedIn) : false;
+  });
+
+  useEffect(() => {
+    // 로그인 상태가 변경될 때 sessionStorage에 저장
+    sessionStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
 
   const handleToggle = () => {
     setLoggedIn((prevLoggedIn) => !prevLoggedIn);
