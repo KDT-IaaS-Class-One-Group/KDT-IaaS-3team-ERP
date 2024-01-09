1615fb87901b101a8478ee47ac997bc0887c3ea0
samuel21695 <samuel21695@gmail.com>
2024-01-09 15:45:10 +0900

    ✨ Feat: Link to User main page

--- jjanbari/src/pages/Admin/AdminPage.tsx
+++ jjanbari/src/pages/Admin/AdminPage.tsx
@@ -1,6 +1,6 @@
 // src/pages/Admin/AdminPage.tsx
-
 import React from "react";
+import { Link } from 'react-router-dom';
 
 
 import './AdminPage.css';
@@ -9,6 +9,9 @@ const AdminPage = () => {
   return (
     <div id="container">
       <h1>관리자 페이지</h1>
+      <Link to="/">
+        <h2>사용자 페이지로 이동</h2>
+      </Link>
     </div>
   );
 };
