0f0088a7295130e1a05bb79837f0dac04e51efd5
Ho Nyeong <dev.honing@gmail.com>
2024-01-09 11:26:21 +0900

    :bug: Fix: 브랜치 병합 중에 누락된 마이 페이지 내용 복구

--- jjanbari/src/Layout/Header/User/UserHeader.tsx
+++ jjanbari/src/Layout/Header/User/UserHeader.tsx
@@ -7,7 +7,7 @@ const UserHeader = () => {
     <a href="/admin">관리자 페이지로</a>
     <a href="/login">로그인 페이지로</a>
     <a href="signup">회원 가입 페이지로</a>
-    <a href="withdraw">회원 탈퇴 페이지로</a>
+    <a href="mypage">마이 페이지로</a>
     <a href="/">메인 페이지로</a>
   </div>;
 };
--- jjanbari/src/Layout/Routes/Router.tsx
+++ jjanbari/src/Layout/Routes/Router.tsx
@@ -5,10 +5,17 @@ import { Routes, Route } from "react-router-dom";
 import AdminPage from "../../pages/Admin/AdminPage";
 import LoginPage from "../../pages/Login/LoginPage";
 import SignupPage from "../../pages/Signup/SignupPage";
-import WithdrawPage from "../../pages/Mypage/Withdraw/WithdrawPage";
+import Mypage from "../../pages/Mypage/Mypage";
 import MainPage from "../../pages/Main/MainPage";
 
 import "./Router.css";
+
+import CartPage from "../../pages/Mypage/Cart/Cart";
+import DeliveryPage from "../../pages/Mypage/Delivery/Delivery";
+import LikePage from "../../pages/Mypage/Like/Like";
+import OrderPage from "../../pages/Mypage/Order/Order";
+import RecentPage from "../../pages/Mypage/Recent/Recent";
+import WithdrawPage from "../../pages/Mypage/Withdraw/WithdrawPage";
 
 const Router = () => {
   return (
@@ -16,8 +23,16 @@ const Router = () => {
       <Route path="/admin" element={<AdminPage />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/signup" element={<SignupPage />}></Route>
-      <Route path="/withdraw" element={<WithdrawPage />}></Route>
+      <Route path="/mypage" element={<Mypage />}></Route>
       <Route path="/" element={<MainPage />}></Route>
+
+      {/* 마이 페이지 내부 */}
+      <Route path="/cart" element={<CartPage />}></Route>
+      <Route path="/delivery" element={<DeliveryPage />}></Route>
+      <Route path="/like" element={<LikePage />}></Route>
+      <Route path="/order" element={<OrderPage />}></Route>
+      <Route path="/recent" element={<RecentPage />}></Route>
+      <Route path="/withdraw" element={<WithdrawPage />}></Route>
     </Routes>
   );
 };
