743b744a53b6ce78764e07f3346b92e5be780b57
samuel21695 <samuel21695@gmail.com>
2024-01-09 14:50:19 +0900

    ✨ Feat: routing resetting

--- jjanbari/src/Layout/Header/Admin/AdminHeader.tsx
+++ jjanbari/src/Layout/Header/Admin/AdminHeader.tsx
@@ -5,8 +5,8 @@ import './AdminHeader.css';
 const AdminHeader = () => {
   return (
     <div id="adminHeader">
-      <a href="/admin">상품 관리</a>
-      <a href="/login">회원 관리</a>
+      <a href="/productManagement">상품 관리</a>
+      <a href="/userManagement">회원 관리</a>
       <a href="/statics">통계 처리</a>
       <a href="/admin">관리자 페이지 로고</a>
     </div>
