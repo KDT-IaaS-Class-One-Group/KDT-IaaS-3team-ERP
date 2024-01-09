9be5e4c9ff7eb678f49e5901d3d32a60083798f9
samuel21695 <samuel21695@gmail.com>
2024-01-09 14:56:46 +0900

    ✨ ProductManagement page update

--- jjanbari/src/pages/Admin/Product/ProductManagementPage.tsx
+++ jjanbari/src/pages/Admin/Product/ProductManagementPage.tsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const ProductManagementPage = () => {
   return (
     <div id="container">
-      <h1>상품 관리</h1>
+      <Link to="/productUpload">
+        <h1>상품 등록</h1>
+      </Link>
+      <Link to="/productRemove">
+        <h1>상품 제거</h1>
+      </Link>
     </div>
   );
 };
--- jjanbari/src/pages/Admin/Product/ProductRemove/ProductRemovePage.tsx
+++ jjanbari/src/pages/Admin/Product/ProductRemove/ProductRemovePage.tsx
@@ -1,7 +1,7 @@
 const ProductRemovePage = () => {
   return (
     <div id="container">
-      <h1>장바구니</h1>
+      <h1>상품 제거</h1>
     </div>
   );
 };
