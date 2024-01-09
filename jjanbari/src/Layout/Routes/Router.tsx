9b37c91d5710d77725260e069499517eec0f306b
samuel21695 <samuel21695@gmail.com>
2024-01-09 12:59:43 +0900

    :pencil2: Rename: file name change & route name change
    파일 이름 변경 후 변경된 파일대로 라우팅 재설정

--- jjanbari/src/Layout/Routes/Router.tsx
+++ jjanbari/src/Layout/Routes/Router.tsx
@@ -10,10 +10,10 @@ import MainPage from "../../pages/Main/MainPage";
 
 import "./Router.css";
 
-import CartPage from "../../pages/Mypage/Cart/Cart";
-import DeliveryPage from "../../pages/Mypage/Delivery/Delivery";
-import LikePage from "../../pages/Mypage/Like/Like";
-import OrderPage from "../../pages/Mypage/Order/Order";
+import CartPage from "../../pages/Mypage/Cart/CartPage";
+import DeliveryPage from "../../pages/Mypage/Delivery/DeliveryPage";
+import LikePage from "../../pages/Mypage/Like/LikePage";
+import OrderPage from "../../pages/Mypage/Order/OrderPage";
 import RecentPage from "../../pages/Mypage/Recent/Recent";
 import WithdrawPage from "../../pages/Mypage/Withdraw/WithdrawPage";
 
