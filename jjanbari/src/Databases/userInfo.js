fc51d99ee965bc98773b1befd766f1c2357a0b26
Ho Nyeong <dev.honing@gmail.com>
2024-01-10 14:28:40 +0900

    :alembic: Test: userInfo.js 리팩토링
    - 연결에 성공한 메시지 출력을 줄이고, 실패했을 때에만 에러 메시지를 출력하도록 변경

--- jjanbari/src/Databases/userInfo.js
+++ jjanbari/src/Databases/userInfo.js
@@ -1,52 +1,55 @@
-// src/Databases/userInfo.js
-
 const mysql = require('mysql2');
+
+const databaseName = 'userInfo';
+const tableName = 'users';
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '1234',
-  database: 'userInfo'
-})
+  database: databaseName
+});
 
 // 데이터베이스를 생성하는 쿼리문
-const createDatabaseQuery = 'CREATE DATABASE IF NOT EXISTS userInfo';
+const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS ${databaseName}`;
 
 // 테이블을 생성하는 쿼리문
 const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL
+  CREATE TABLE IF NOT EXISTS ${tableName} (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
   );
 `;
 
-// MariaDB 서버에 연결하면, userInfo DB 생성 및 users TABLE 생성
+// MariaDB 서버에 연결
 connection.connect((error) => {
   if (error) {
-    console.error('userInfo DB 연결 실패: ', error.message);
-  } else {
-    console.log('userInfo DB 연결 성공');
-
-    // 데이터베이스 생성 쿼리 실행
-    connection.query(createDatabaseQuery, (dbError) => {
-      if (dbError) {
-        console.error('userInfo DB 생성 실패: ', dbError.message);
-      } else {
-        console.log('userInfo DB 생성 성공 또는 이미 존재');
-
-        // 테이블 생성 쿼리 실행
-        connection.query(createTableQuery, (tableError) => {
-          if (tableError) {
-            console.error('users TABLE 생성 실패: ', tableError.message);
-          } else {
-            console.log('users TABLE 생성 성공 또는 이미 존재');
-          }
-        });
-      }
-    });
+    console.error(`${databaseName} DB 연결 실패: `, error.message);
+    return;
   }
+
+  // 데이터베이스 생성 쿼리 실행
+  connection.query(createDatabaseQuery, (dbError) => {
+    if (dbError) {
+      console.error(`${databaseName} DB 생성 실패: `, dbError.message);
+      connection.end(); // 에러 발생 시 연결 종료
+      return;
+    }
+
+    // 테이블 생성 쿼리 실행
+    connection.query(createTableQuery, (tableError) => {
+      if (tableError) {
+        console.error(`${tableName} TABLE 생성 실패: `, tableError.message);
+      } else {
+        console.log(`[${databaseName}].${tableName} 초기화 완료`);
+      }
+
+      // 연결 종료
+      connection.end();
+    });
+  });
 });
 
 module.exports = connection;
