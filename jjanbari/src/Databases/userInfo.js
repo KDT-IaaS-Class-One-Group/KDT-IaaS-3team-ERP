4af53c5695ff075887f60f0d69567a120f273b70
Ho Nyeong <dev.honing@gmail.com>
2024-01-10 16:55:27 +0900

    :bug: Fix: userInfo.js 버그 수정
    - 회원가입 절차 확인 완료
    - 로그인 절차 확인 완료

--- jjanbari/src/Databases/userInfo.js
+++ jjanbari/src/Databases/userInfo.js
@@ -1,47 +1,52 @@
 // src/Databases/userInfo.js
 
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-const databaseName = 'userInfo';
-const tableName = 'users';
-
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '1234',
-  database: databaseName
-});
+  database: 'userInfo'
+})
 
 // 데이터베이스를 생성하는 쿼리문
-const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS ${databaseName}`;
+const createDatabaseQuery = 'CREATE DATABASE IF NOT EXISTS userInfo';
 
 // 테이블을 생성하는 쿼리문
 const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS ${tableName} (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL
+  CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  userID VARCHAR(255) NOT NULL,
+  userPW VARCHAR(255) NOT NULL,
+  userNAME VARCHAR(255) NOT NULL
   );
 `;
 
-// 데이터베이스 초기화 함수
-async function initializeDatabase() {
-  try {
+// MariaDB 서버에 연결하면, userInfo DB 생성 및 users TABLE 생성
+connection.connect((error) => {
+  if (error) {
+    console.error('userInfo DB 연결 실패: ', error.message);
+  } else {
+    console.log('userInfo DB 연결 성공');
+
     // 데이터베이스 생성 쿼리 실행
-    await pool.query(createDatabaseQuery);
+    connection.query(createDatabaseQuery, (dbError) => {
+      if (dbError) {
+        console.error('userInfo DB 생성 실패: ', dbError.message);
+      } else {
+        console.log('userInfo DB 생성 성공 또는 이미 존재');
 
-    // 테이블 생성 쿼리 실행
-    await pool.query(createTableQuery);
-
-    console.log(`초기화 완료!\n - DB명: ${databaseName}\n - TABLE명: ${tableName}`);
-  } catch (error) {
-    console.error('초기화 실패: ', error.message);
-  } finally {
-    // 풀 연결 해제
-    pool.end();
+        // 테이블 생성 쿼리 실행
+        connection.query(createTableQuery, (tableError) => {
+          if (tableError) {
+            console.error('users TABLE 생성 실패: ', tableError.message);
+          } else {
+            console.log('users TABLE 생성 성공 또는 이미 존재');
+          }
+        });
+      }
+    });
   }
-}
+});
 
-// 데이터베이스 초기화 함수 호출
-initializeDatabase();
+module.exports = connection;
