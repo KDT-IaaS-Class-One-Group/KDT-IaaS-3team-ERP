67c85999a47d3af3bd4f8f9e297047f66d9adba9
Yusmin97 <tmdamsi97@gmail.com>
2024-01-10 18:16:29 +0900

    🔨 Modify: 수정된 userInfo 모듈 변경
    connection에서 userQuery로 변경

--- jjanbari/server.js
+++ jjanbari/server.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const connection = require('./src/Databases/userInfo');
-const {query} = require('./src/Databases/productInfo');
+const { userQuery } = require('./src/Databases/userInfo');
+const { productQuery } = require('./src/Databases/productInfo');
 
 const app = express();
 const port = 3001;
@@ -13,33 +13,19 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // 회원 가입 라우트
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   try {
     const { userID, userPW, userNAME } = req.body;
 
-    // 아이디로 사용자 조회 관련 내용 삭제
-
-    // 사용자 정보 저장
     const insertDataQuery = `
       INSERT INTO users (userID, userPW, userNAME)
       VALUES (?, ?, ?);
     `;
 
-    connection.query(
-      insertDataQuery,
-      [userID, userPW, userNAME],
-      (error, results) => {
-        if (error) {
-          console.error("회원 가입 실패:", error);
-          res
-            .status(500)
-            .send("회원 가입에 실패했습니다. 다시 시도해주세요.");
-        } else {
-          console.log("회원 가입 정보 저장 성공:", req.body);
-          res.sendStatus(200);
-        }
-      }
-    );
+    await userQuery(insertDataQuery, [userID, userPW, userNAME]);
+
+    console.log("회원 가입 정보 저장 성공:", req.body);
+    res.sendStatus(200);
   } catch (error) {
     console.error("회원 가입 실패:", error);
     res.status(500).send("회원 가입에 실패했습니다. 다시 시도해주세요.");
@@ -50,47 +36,35 @@ app.post("/signup", (req, res) => {
 app.post('/login', async (req, res) => {
   try {
     const { userID, userPW } = req.body;
-    
-    // 아이디로 사용자 조회
-    connection.query('SELECT * FROM users WHERE userID = ?', [userID], async (error, results) => {
-      if (error) {
-        console.error('로그인 실패:', error);
-        res.status(500).send('로그인에 실패했습니다. 다시 시도해주세요.');
-        return;
-      }
 
-      if (results.length > 0) {
-        const user = results[0];
+    const results = await userQuery('SELECT * FROM users WHERE userID = ?', [userID]);
 
-        // 비밀번호 비교
-        if (user.userPW === userPW) {
-          if (user.userID === 'adroot') {
-            // 관리자 로그인 성공
-            console.log('관리자로 로그인하였습니다.');
-            res.status(201).json({ role: 'admin' });
-          } else {
-            // 사용자 로그인 성공
-            console.log('사용자로 로그인하였습니다.');
-            res.status(200).json({ role: 'user' });
-          }
+    if (results.length > 0) {
+      const user = results[0];
+
+      // 비밀번호 비교
+      if (user.userPW === userPW) {
+        if (user.userID === 'adroot') {
+          // 관리자 로그인 성공
+          console.log('관리자로 로그인하였습니다.');
+          res.status(201).json({ role: 'admin' });
         } else {
-          console.error('로그인 실패: 비밀번호가 일치하지 않습니다.');
-          res.status(401).send('비밀번호가 일치하지 않습니다.');
+          // 사용자 로그인 성공
+          console.log('사용자로 로그인하였습니다.');
+          res.status(200).json({ role: 'user' });
         }
       } else {
-        console.error('로그인 실패: 해당 ID가 존재하지 않습니다.');
-        res.status(401).send('해당 ID가 존재하지 않습니다.');
+        console.error('로그인 실패: 비밀번호가 일치하지 않습니다.');
+        res.status(401).send('비밀번호가 일치하지 않습니다.');
       }
-    });
+    } else {
+      console.error('로그인 실패: 해당 ID가 존재하지 않습니다.');
+      res.status(401).send('해당 ID가 존재하지 않습니다.');
+    }
   } catch (error) {
     console.error('로그인 실패:', error);
     res.status(500).send('로그인에 실패했습니다. 다시 시도해주세요.');
   }
-});
-
-// 관리자 페이지 라우트
-app.get('/admin', (req, res) => {
-  res.send('관리자 페이지입니다.');
 });
 
 app.post('/addProducts', async (req, res) => {
@@ -98,14 +72,14 @@ app.post('/addProducts', async (req, res) => {
 
   try {
     // 먼저 동일한 name과 price를 가진 상품이 있는지 확인합니다.
-    const products = await query('SELECT * FROM products WHERE name = ? AND price = ?', [name, price]);
+    const products = await productQuery('SELECT * FROM products WHERE name = ? AND price = ?', [name, price]);
 
     if (products.length > 0) {
       // 동일한 name과 price를 가진 상품이 이미 있으면, 해당 상품의 quantity를 업데이트합니다.
-      await query('UPDATE products SET quantity = quantity + ? WHERE name = ? AND price = ?', [quantity, name, price]);
+      await productQuery('UPDATE products SET quantity = quantity + ? WHERE name = ? AND price = ?', [quantity, name, price]);
     } else {
       // 동일한 name과 price를 가진 상품이 없으면, 새로운 상품을 추가합니다.
-      await query('INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)', [name, price, quantity]);
+      await productQuery('INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)', [name, price, quantity]);
     }
 
     res.json({ success: true });
@@ -117,7 +91,7 @@ app.post('/addProducts', async (req, res) => {
 
 app.get('/products', async (req, res) => {
   try {
-    const products = await query('SELECT * FROM products');
+    const products = await productQuery('SELECT * FROM products');
     res.json(products);
   } catch (error) {
     console.error('Error during fetching products:', error.message);
@@ -130,7 +104,7 @@ app.put('/products/:id', async (req, res) => {
   const { quantity } = req.body;
 
   try {
-    await query('UPDATE products SET quantity = quantity - ? WHERE id = ?', [quantity, id]);
+    await productQuery('UPDATE products SET quantity = quantity - ? WHERE id = ?', [quantity, id]);
     res.json({ success: true });
   } catch (error) {
     console.error('Error during updating product:', error.message);
@@ -141,7 +115,7 @@ app.put('/products/:id', async (req, res) => {
 // 관리자 페이지 상품 관리
 app.get('/admin/products', async (req, res) => {
   try {
-    const products = await query('SELECT * FROM products');
+    const products = await productQuery('SELECT * FROM products');
     res.json(products);
   } catch (error) {
     console.error('Error during fetching products:', error.message);
@@ -154,7 +128,7 @@ app.put('/admin/products/:id', async (req, res) => {
   const { name, price, quantity } = req.body;
 
   try {
-    await query('UPDATE products SET name = ?, price = ?, quantity = ? WHERE id = ?', [name, price, quantity, id]);
+    await productQuery('UPDATE products SET name = ?, price = ?, quantity = ? WHERE id = ?', [name, price, quantity, id]);
     res.json({ success: true });
   } catch (error) {
     console.error('Error during updating product:', error.message);
@@ -166,7 +140,7 @@ app.delete('/admin/products/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    await query('DELETE FROM products WHERE id = ?', [id]);
+    await productQuery('DELETE FROM products WHERE id = ?', [id]);
     res.json({ success: true });
   } catch (error) {
     console.error('Error during deleting product:', error.message);
@@ -177,7 +151,7 @@ app.delete('/admin/products/:id', async (req, res) => {
 // 관리자 페이지 회원 정보 관리
 app.get('/admin/userprofiles', async (req, res) => {
   try {
-    const userprofiles = await connection.query('SELECT * FROM users');
+    const userprofiles = await userQuery('SELECT * FROM users');
     res.json(userprofiles);
   } catch (error) {
     console.error('Error during fetching users:', error.message);
