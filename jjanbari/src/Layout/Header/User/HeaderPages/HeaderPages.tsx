a7559e6d3f3cf46f404b0d6489ffddff6a5ac300
samuel21695 <samuel21695@gmail.com>
2024-01-10 14:32:45 +0900

    :twisted_rightwards_arrows: Merge: fix conflicts
    conflict 충돌 해결

--- jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
+++ jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
@@ -1,7 +1,12 @@
 import './HeaderPages.css'
+import React, { useNavigate } from 'react-router-dom';
+
 import LoginToggle from './LoginStatus/LoginToggle';
+import { isLoggedIn } from './LoginStatus/isLoggedIn';
 
 const HeaderPages = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="headerPages">
       <div className='loginStatus'>
@@ -10,8 +15,10 @@ const HeaderPages = () => {
       <div className='pages'>
         <a href="/cart">장바구니</a>
         <a href="/like">좋아요</a>
-        <a href="/mypage">마이페이지</a>
-      </div>
+        <a href="/mypage" onClick={(event) => {
+        event.preventDefault();
+        navigate(isLoggedIn() ? '/mypage' : '/login');
+      }}>마이페이지</a>      </div>
     </div>
   );
 };
--- jjanbari/src/Layout/Header/User/UserHeader.tsx
+++ jjanbari/src/Layout/Header/User/UserHeader.tsx
@@ -1,12 +1,9 @@
 // src/Component/Header/UserHeader.tsx
 
-import React, { useNavigate } from 'react-router-dom';
-import LoginToggle from './LoginStatus/LoginToggle';
-import { isLoggedIn } from './LoginStatus/isLoggedIn';
+import React from 'react-router-dom';
+
 
 import './UserHeader.css'
-import React from 'react';
-import './UserHeader.css';
 // import LoginToggle from './LoginStatus/LoginToggle';
 import HeaderLogo from './HeaderLogo/HeaderLogo';
 import HeaderPages from './HeaderPages/HeaderPages';
@@ -14,17 +11,8 @@ import HeaderCategory from './HeaderCategory/HeaderCategory';
 
 // 전체 헤더를 구성하는 컴포넌트
 const UserHeader = () => {
-  const navigate = useNavigate();
 
   return (
-    <div id="userHeader">
-      <LoginToggle />
-      <a href="/">로고</a>
-      <a href="/mypage" onClick={(event) => {
-        event.preventDefault();
-        navigate(isLoggedIn() ? '/mypage' : '/login');
-      }}>마이페이지</a>
-    </div>
     <header id="userHeader">
       <HeaderPages />
       <HeaderLogo />
