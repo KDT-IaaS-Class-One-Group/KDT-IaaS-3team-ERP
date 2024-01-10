abeafbdeec3d11650c6d3955e8873a5e77edf89f
ChaeYi <chaeyi990731@gmail.com>
2024-01-10 11:31:25 +0900

    :sparkles: Feat: 카테고리 컴포넌트 분리

---
+++ jjanbari/src/Layout/Header/User/HeaderCategory.tsx
@@ -0,0 +1,14 @@
+// 카테고리를 표시하는 컴포넌트
+const HeaderCategory = () => {
+  return (
+    <div className="headerCategories">
+      <div>
+        <div>카테고리</div>
+        <div>고양이</div>
+        <div>강아지</div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderCategory;
