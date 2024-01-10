722bd09dfea88fa209fc605398808f58806b74f3
Yusmin97 <tmdamsi97@gmail.com>
2024-01-10 11:57:47 +0900

    ✏️ Rename: 상품 제거 페이지 상품 수정으로 이름 변경

--- jjanbari/src/pages/Admin/Product/ProductRemove/ProductRemovePage.tsx
+++
@@ -1,9 +0,0 @@
-const ProductRemovePage = () => {
-  return (
-    <div id="container">
-      <h1>상품 제거</h1>
-    </div>
-  );
-};
-
-export default ProductRemovePage;
---
+++ jjanbari/src/pages/Admin/Product/ProductUpdate/ProductUpdatePage.tsx
@@ -0,0 +1,9 @@
+const ProductUpdatePage = () => {
+  return (
+    <div id="container">
+      <h1>상품 수정</h1>
+    </div>
+  );
+};
+
+export default ProductUpdatePage;
