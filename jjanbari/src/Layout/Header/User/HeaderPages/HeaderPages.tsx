92393e029dbe923ee5217d9a9de58a5a60ff4a4e
samuel21695 <samuel21695@gmail.com>
2024-01-10 12:58:38 +0900

    :art: Design: page styling change-2
    스타일링 체인지

--- jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
+++ jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
@@ -4,9 +4,14 @@ import LoginToggle from './LoginStatus/LoginToggle';
 const HeaderPages = () => {
   return (
     <div className="headerPages">
-      <a href="/cart">장바구니</a>
-      <a href="/like">좋아요</a>
-      <a href="/mypage">마이페이지</a>
+      <div className='loginStatus'>
+        <LoginToggle />
+      </div>
+      <div className='pages'>
+        <a href="/cart">장바구니</a>
+        <a href="/like">좋아요</a>
+        <a href="/mypage">마이페이지</a>
+      </div>
     </div>
   );
 };
