89e796b5e551d3f7460a260184e161e338de04b6
samuel21695 <samuel21695@gmail.com>
2024-01-10 14:54:28 +0900

    :art: Design: logo style added
    로고 사진 배경으로 추가

--- jjanbari/src/Layout/Header/User/HeaderLogo/HeaderLogo.tsx
+++ jjanbari/src/Layout/Header/User/HeaderLogo/HeaderLogo.tsx
@@ -3,7 +3,10 @@ import './HeaderLogo.css'
 const HeaderLogo = () => {
   return (
     <div className="headerLogo">
-      <a href="/">로고</a> {/* 실제 로고 이미지나 텍스트로 교체하세요 */}
+      <a href="/">
+        <div className='logoImage'>
+        </div>
+      </a> {/* 실제 로고 이미지나 텍스트로 교체하세요 */}
     </div>
   );
 };
