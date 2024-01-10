b6cecfc08eae57f11ecea65960ffd55b2ba5efab
samuel21695 <samuel21695@gmail.com>
2024-01-10 11:49:54 +0900

    📄 Docs: subfolder created

--- jjanbari/src/Layout/Header/User/UserHeader.tsx
+++ jjanbari/src/Layout/Header/User/UserHeader.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './UserHeader.css';
 // import LoginToggle from './LoginStatus/LoginToggle';
-import HeaderLogo from './HeaderLogo';
-import HeaderPages from './HeaderPages';
-import HeaderCategory from './HeaderCategory';
+import HeaderLogo from './HeaderLogo/HeaderLogo';
+import HeaderPages from './HeaderPages/HeaderPages';
+import HeaderCategory from './HeaderCategory/HeaderCategory';
 
 // 전체 헤더를 구성하는 컴포넌트
 const UserHeader = () => {
