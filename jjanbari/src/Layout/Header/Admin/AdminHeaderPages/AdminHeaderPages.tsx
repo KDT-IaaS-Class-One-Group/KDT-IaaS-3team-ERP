13269b19a6b4e911b2534bf1add18aeb98e083be
samuel21695 <samuel21695@gmail.com>
2024-01-11 20:29:55 +0900

    ✨Feat: link to main page 메인 페이지로 이동

--- jjanbari/src/Layout/Header/Admin/AdminHeaderPages/AdminHeaderPages.tsx
+++ jjanbari/src/Layout/Header/Admin/AdminHeaderPages/AdminHeaderPages.tsx
@@ -6,7 +6,8 @@ const AdminHeaderPages = () => {
     <div className="adminHeaderPages">
       <a href="/productManagement">상품 관리</a>
       <a href="/userManagement">유저 관리</a>
-      <a href="/statistics">통계 처리</a>      
+      <a href="/statistics">통계 처리</a>     
+      <a href="/" className='main'>메인 페이지로</a> 
     </div>
   );
 };
