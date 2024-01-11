7b87d4bab1369b18bc4be7c49cd063b339aeebae
samuel21695 <samuel21695@gmail.com>
2024-01-11 19:54:36 +0900

    🎨 Design: AdminHeader styling
    AdminHeader 테마 변경

--- jjanbari/src/Layout/Header/Admin/AdminHeader.tsx
+++ jjanbari/src/Layout/Header/Admin/AdminHeader.tsx
@@ -1,14 +1,15 @@
 // src/Component/Header/Admin/AdminHeader.tsx
-import HeaderPages from '../User/HeaderPages/HeaderPages';
 import HeaderLogo from '../User/HeaderLogo/HeaderLogo';
 import HeaderCategory from '../User/HeaderCategory/HeaderCategory';
+import AdminHeaderPages from './AdminHeaderPages/AdminHeaderPages';
+
 import './AdminHeader.css';
 
 const AdminHeader = () => {
   return (
     <header id="AdminHeader">
       <HeaderLogo />
-      <HeaderCategory />
+      <AdminHeaderPages />
     </header>
   );
 };
---
+++ jjanbari/src/Layout/Header/Admin/AdminHeaderPages/AdminHeaderPages.tsx
@@ -0,0 +1,14 @@
+import './AdminHeaderPages.css'
+
+const AdminHeaderPages = () => {
+
+  return (
+    <div className="adminHeaderPages">
+      <a href="/cart">장바구니</a>
+      <a href="/like">좋아요</a>
+      <a href="/mypage">마이페이지</a>      
+    </div>
+  );
+};
+
+export default AdminHeaderPages;
