bcdea89ab4bd534036ec20eda82204c6efb23443
samuel21695 <samuel21695@gmail.com>
2024-01-11 11:37:45 +0900

    :hammer: Modify: classname added on each products
    classname 추가

--- jjanbari/src/pages/Main/ProductRender.tsx
+++ jjanbari/src/pages/Main/ProductRender.tsx
@@ -34,7 +34,7 @@ const ProductRender = () => {
     <div className="product-container">
        {products.length > 0 &&
         products.map((product) => (
-          <div key={product.id}>
+          <div className="products" key={product.id}>
             <h2>{product.name}</h2>
             <p>가격: {product.price}</p>
             <p>수량: {product.quantity}</p>
