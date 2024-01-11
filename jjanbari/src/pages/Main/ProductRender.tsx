3096bbb6cdabddb3d17766242dd3abb81ab8c1d0
samuel21695 <samuel21695@gmail.com>
2024-01-11 12:45:19 +0900

    :art: Design: contents layout apply
    레이아웃 적용중 그런데 상품 많이 등록하면 contents 에서 삐져 나오는거 해결 하는중

--- jjanbari/src/pages/Main/ProductRender.tsx
+++ jjanbari/src/pages/Main/ProductRender.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import handlePurchase from './function/HandlePurchase';
 import { isLoggedIn } from '../../Layout/Header/User/HeaderPages/LoginStatus/isLoggedIn';
 
 type Product = {
