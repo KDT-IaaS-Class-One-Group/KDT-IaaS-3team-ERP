be133d645ebd317bbcaeb3c4f107cbbca7d3846e
Ho Nyeong <dev.honing@gmail.com>
2024-01-12 11:39:13 +0900

    :sparkles: Feat: 로그인 토글 기본로직 작성

--- jjanbari/src/pages/Login/LoginForm.tsx
+++ jjanbari/src/pages/Login/LoginForm.tsx
@@ -1,36 +1,42 @@
-// src/pages/Login/LoginForm.tsx
+// src/components/LoginForm.tsx
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginForm: React.FC = () => {
+  // 상태값은 boolean 타입으로 초기화해서, 로그인 상태를 마치 스위치 켜듯 토글할 수 있게 조정
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 로그인 처리를 담당하는 함수
+  const handleLogin = () => {
+    // 로그인 처리 로직
+
+    // 로그인 상태 토글
+    setIsLoggedIn((prev) => !prev);
+  };
+
   return (
     <form className="LoginForm">
       <label>
         아이디:
-        <input
-          type="text"
-          name="userID"
-          required
-        />
+        <input type="text" name="userID" required />
       </label>
       <br />
       <label>
         비밀번호:
-        <input
-          type="password"
-          name="userPW"
-          required
-        />
+        <input type="password" name="userPW" required />
       </label>
       <br />
       <div>
         <div className="signup">
-          <Link to="/signup" className='sign'>
+          <Link to="/signup" className="sign">
             회원가입
           </Link>
         </div>
-        <button type="submit">로그인</button>
+        <button type="button" onClick={handleLogin}>
+          {isLoggedIn ? "로그아웃" : "로그인"}
+        </button>
+        {isLoggedIn && <p>로그인 중입니다.</p>}
       </div>
     </form>
   );
