02155c08b5c53695781d3876446dab7e31c442ed
Ho Nyeong <dev.honing@gmail.com>
2024-01-13 22:50:42 +0900

    :poop: Bad: 서버 에러
    - 초기화 실패:  connect ECONNREFUSED ::1:3306

--- jjanbari/src/pages/Login/LoginForm.tsx
+++ jjanbari/src/pages/Login/LoginForm.tsx
@@ -1,18 +1,6 @@
 // src/pages/Login/LoginForm.tsx
 
-/*
-  LoginForm 컴포넌트는 로그인 폼을 정의하고, 로그인 상태와 역할을 관리합니다.
-  handleLoginSuccess 함수는 로그인에 성공하면 수행할 로직을 처리하고, 역할에 따라 페이지를 이동합니다.
-  handleLogout 함수는 로그아웃 버튼을 클릭하면 로그아웃 로직을 처리합니다.
- */
-
-  /* 
-    <Form 내부>
-    Form 내부에는 아이디와 비밀번호 입력 필드, 회원가입 링크, 로그인 버튼이 있습니다.
-    onSubmit 이벤트에서는 handleSubmit 함수를 호출하여 실제 로그인 로직을 처리합니다.
-  */
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import handleSubmit from "./function/HandleSubmit";
 
@@ -27,14 +15,25 @@ const LoginForm: React.FC = () => {
 
   const navigate = useNavigate();
 
-  const handleLoginSuccess = (role: string) => {
-    // 로그인 성공 시 수행할 로직
-    console.log(`로그인 성공! 역할: ${role}`);
-    // 예: 다음 페이지로 이동 등
+  useEffect(() => {
+    // 페이지 로딩 시 로컬 스토리지에서 정보 가져오기
+    const storedIsLoggedIn = localStorage.getItem("isLoggedIn");
+    const storedUserRole = localStorage.getItem("userRole");
 
+    if (storedIsLoggedIn && storedUserRole) {
+      setIsLoggedIn(true);
+      setUserRole(storedUserRole);
+    }
+  }, []);
+
+  const handleLoginSuccess = (role: string) => {
     // 로그인 상태와 역할 업데이트
     setIsLoggedIn(true);
     setUserRole(role);
+
+    // 로컬 스토리지에 정보 저장
+    localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("userRole", role);
 
     // 역할에 따라 다른 경로로 이동
     if (role === "admin") {
@@ -48,9 +47,11 @@ const LoginForm: React.FC = () => {
     // 로그아웃 버튼 클릭 시 로직
     setIsLoggedIn(false);
     setUserRole("");
-    // 예: 로그아웃 성공 시 로직 추가
-  };
 
+    // 로컬 스토리지에서 정보 삭제
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("userRole");
+  };
 
   return (
     <form className="LoginForm" onSubmit={handleSubmit(loginFormData, handleLoginSuccess)}>
@@ -83,8 +84,6 @@ const LoginForm: React.FC = () => {
           </Link>
         </div>
         <button type="submit">로그인</button>
-        {/* 로그인 중인 경우 메시지 표시 */}
-        {isLoggedIn && <p>로그인 중입니다.</p>}
       </div>
     </form>
   );
