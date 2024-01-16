c5ed5b40f178b2fe5427da1cca6de7821f39f243
ChaeYi <chaeyi990731@gmail.com>
2024-01-16 11:28:40 +0900

    :sparkles: Feat: 장바구니 로직 추가

--- jjanbari/src/pages/Main/ProductRender.tsx
+++ jjanbari/src/pages/Main/ProductRender.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { isLoggedIn } from '../../Layout/Header/User/HeaderPages/LoginStatus/isLoggedIn';
+import addCart from './function/addCart';
 
 type Product = {
   id: string;
@@ -21,6 +22,26 @@ const ProductRender = () => {
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const getCurrentUserId = (): string | null => {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user).id : null;
+  };
+
+  const handleAddToCartClick = async (product: Product) => {
+    if (!isLoggedIn()) {
+      navigate('/login');
+      return;
+    }
+
+    const userId = getCurrentUserId();
+    const quantity = Number(document.getElementById(`quantity-${product.id}`)?.value || 0);
+
+    const success = await addCart(product.id, quantity, userId);
+    if (!success) {
+      alert('장바구니 추가 실패');
+    }
+  };
 
   const handleBuy = (product: Product) => {
     // 선택한 수량을 가져옵니다.
@@ -42,13 +63,13 @@ const ProductRender = () => {
         products.map((product) => (
           <div className="product-item" key={product.id}>
             <img src={product.img} alt={product.name} /> {/* 이미지 렌더링 */}
-            <div className='product-details'>
+            <div className="product-details">
               <h3>{product.name}</h3>
               <br></br>
               <p>가격: {product.price}</p>
               <p>수량: {product.quantity}</p>
               <input type="number" id={`quantity-${product.name}`} min="1" max={product.quantity} />
-              <button>장바구니</button>
+              <button onClick={() => handleAddToCartClick(product)}>장바구니</button>
               <button>좋아요</button>
               <button onClick={() => handleBuy(product)}>바로구매</button>
             </div>
@@ -56,5 +77,5 @@ const ProductRender = () => {
         ))}
     </div>
   );
-}
+};
 export default ProductRender;
