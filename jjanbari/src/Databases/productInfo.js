992c0d725bd60a116353fec04a0e5652e2f36498
Ho Nyeong <dev.honing@gmail.com>
2024-01-17 11:15:45 +0900

    :pencil2: Style: Prettier를 통한 코드 스타일 정리

--- jjanbari/src/Databases/productInfo.js
+++ jjanbari/src/Databases/productInfo.js
@@ -1,21 +1,21 @@
 // src/Databases/productInfo.js
 
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
-const databaseName = 'productInfo';
-const tableNameP = 'products';
-const tableNameAC = 'animal_categories';
-const tableNameAC2 = 'age_categories';
-const tableNameFC = 'functional_categories';
-const tableNameAP = 'animal_products';
-const tableNameAP2 = 'age_products';
-const tableNameFP = 'functional_products';
+const databaseName = "productInfo";
+const tableNameP = "products";
+const tableNameAC = "animal_categories";
+const tableNameAC2 = "age_categories";
+const tableNameFC = "functional_categories";
+const tableNameAP = "animal_products";
+const tableNameAP2 = "age_products";
+const tableNameFP = "functional_products";
 
 // 데이터베이스 연결 풀 생성
 let pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
+  host: "localhost",
+  user: "root",
+  password: "1234",
   database: databaseName,
 });
 
@@ -38,15 +38,15 @@ async function initializeDatabase() {
         age_id INT PRIMARY KEY AUTO_INCREMENT,
         age_name VARCHAR(255) NOT NULL
       );
-    `)
+    `);
 
     await pool.query(`
       CREATE TABLE IF NOT EXISTS ${tableNameFC} (
         functional_id INT PRIMARY KEY AUTO_INCREMENT,
         functional_name VARCHAR(255) NOT NULL
       );
-    `)
-    
+    `);
+
     await pool.query(`
       CREATE TABLE IF NOT EXISTS ${tableNameP} (
         product_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -71,7 +71,7 @@ async function initializeDatabase() {
         FOREIGN KEY (product_id) REFERENCES products(product_id),
         FOREIGN KEY (animal_id) REFERENCES animal_categories(animal_id)
       );
-    `)
+    `);
 
     await pool.query(`
       CREATE TABLE IF NOT EXISTS ${tableNameAP2} (
@@ -81,7 +81,7 @@ async function initializeDatabase() {
         FOREIGN KEY (product_id) REFERENCES products(product_id),
         FOREIGN KEY (age_id) REFERENCES age_categories(age_id)
       );
-    `)
+    `);
 
     await pool.query(`
       CREATE TABLE IF NOT EXISTS ${tableNameFP} (
@@ -91,7 +91,7 @@ async function initializeDatabase() {
         FOREIGN KEY (product_id) REFERENCES products(product_id),
         FOREIGN KEY (functional_id) REFERENCES functional_categories(functional_id)
       );
-    `)
+    `);
 
     //payment 테이블 생성 쿼리
     await pool.query(`
@@ -104,9 +104,11 @@ async function initializeDatabase() {
     );
   `);
 
-    console.log(`초기화 완료!\n - DB명: ${databaseName}\n - TABLE명: ${tableNameP}, ${tableNameAC}, ${tableNameAC2}, ${tableNameFC}, ${tableNameAP}, ${tableNameAP2}, ${tableNameFP}`);
+    console.log(
+      `초기화 완료!\n - DB명: ${databaseName}\n - TABLE명: ${tableNameP}, ${tableNameAC}, ${tableNameAC2}, ${tableNameFC}, ${tableNameAP}, ${tableNameAP2}, ${tableNameFP}`
+    );
   } catch (error) {
-    console.error('초기화 실패: ', error.message);
+    console.error("초기화 실패: ", error.message);
   }
 }
 
@@ -117,12 +119,12 @@ initializeDatabase();
 async function productQuery(sql, params) {
   // 쿼리 실행 전에 연결 상태 체크
   if (pool._closed) {
-    console.error('Pool is closed. Reconnecting...');
+    console.error("Pool is closed. Reconnecting...");
     // 연결이 닫혔다면 새로운 연결 생성
     pool = mysql.createPool({
-      host: 'localhost',
-      user: 'root',
-      password: '1234',
+      host: "localhost",
+      user: "root",
+      password: "1234",
       database: databaseName,
     });
   }
