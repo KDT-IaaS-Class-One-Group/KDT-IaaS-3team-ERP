e5e219f41a9c8126064b406298c129fe9816c16d
Ho Nyeong <dev.honing@gmail.com>
2024-01-18 13:11:15 +0900

    :alembic: Test: 내 프로필을 입력하고, test.js의 콘솔 출력 확인

--- jjanbari/test.js
+++ jjanbari/test.js
@@ -13,17 +13,39 @@ const db = mysql.createConnection({
   database: 'jjanbariERP',
 });
 
+// TEST 테이블 추가
+db.query(`CREATE TABLE IF NOT EXISTS TEST (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  age INT,
+  email VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`)
+
 // Express 서버 라우트 설정
 app.get('/', (req, res) => {
-  // MariaDB에서 데이터 가져오기
+  // MariaDB에서 데이터 가져오기 예제
   db.query('SELECT * FROM TEST', (error, results) => {
     if (error) throw error;
 
-    // 테이블 형태로 콘솔에 출력
+    // 테이블 형태로 결과 콘솔에 출력
     console.table(results);
 
     res.json(results);
   });
+});
+
+// 내 정보를 추가
+const myProfile = {
+  name: 'BHN',
+  age: 26,
+  email: 'dev.honing@gmail.com',
+};
+
+db.query('INSERT INTO TEST SET ?', myProfile, (error, result) => {
+  if (error) throw error;
+  console.log('New data added:', result);
 });
 
 // 서버 실행
