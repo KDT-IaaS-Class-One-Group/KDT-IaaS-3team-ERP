2bfd2f8e19c3118dd51fdb95fc425dc357782b00
Ho Nyeong <dev.honing@gmail.com>
2024-01-18 18:27:47 +0900

    :sparkles: Feat: 로그인 되지 않은 상태에서 접근하면 로그인 페이지로 이동하도록 개선

--- jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
+++ jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
@@ -1,25 +1,44 @@
-import './HeaderPages.css';
-import React, { Link } from 'react-router-dom';
+import "./HeaderPages.css";
+import React, { Link, useNavigate } from "react-router-dom";
 
-import LoginToggle from './LoginStatus/LoginToggle';
+import { isLoggedIn } from "./LoginStatus/isLoggedIn";
+import LoginToggle from "./LoginStatus/LoginToggle";
 
 const HeaderPages = () => {
+  const navigate = useNavigate();
   return (
     <div className="headerPages">
       <div className="loginStatus">
         <LoginToggle />
       </div>
       <div className="pages">
-        <Link to="/cart">장바구니</Link>
-        <Link to="/like">좋아요</Link>
+        <Link
+          to="/cart"
+          onClick={(event) => {
+            event.preventDefault();
+            navigate(isLoggedIn() ? "/cart" : "/login");
+          }}
+        >
+          장바구니
+        </Link>
+        <Link
+          to="/like"
+          onClick={(event) => {
+            event.preventDefault();
+            navigate(isLoggedIn() ? "/like" : "/login");
+          }}
+        >
+          좋아요
+        </Link>
         <Link
           to="/mypage"
           onClick={(event) => {
             event.preventDefault();
+            navigate(isLoggedIn() ? "/mypage" : "/login");
           }}
         >
           마이페이지
-        </Link>{' '}
+        </Link>{" "}
       </div>
     </div>
   );
