0b4dc786953dd5bc5ce8417cd768fe1df0b85f83
ChaeYi <chaeyi990731@gmail.com>
2024-01-22 17:55:12 +0900

    :hammer: Modify: id를 세션으로 가져와서 수정

--- jjanbari/src/pages/Main/function/handleAddToCart.tsx
+++ jjanbari/src/pages/Main/function/handleAddToCart.tsx
@@ -1,25 +1,28 @@
 // src/pages/Main/function/handleAddToCart.tsx
 
 import { NavigateFunction } from 'react-router-dom';
-import { isLoggedIn } from '../../../Layout/Header/User/HeaderPages/LoginStatus/isLoggedIn';
 import { Product } from '../../interface/interface';
 
 const handleAddToCart = async (product: Product, navigate: NavigateFunction) => {
-  // 선택한 수량을 가져옵니다.
-  const selectedQuantity = Number((document.getElementById(`quantity-${product.name}`) as HTMLInputElement).value);
+  const quantityInput = document.getElementById(`quantity-${product.product_id}`) as HTMLInputElement;
+  const selectedQuantity = quantityInput ? Number(quantityInput.value) : 0;
 
-  // 선택한 수량을 포함한 새로운 상품 객체를 생성합니다.
-  const selectedProduct = { ...product, quantity: selectedQuantity };
+  // 세션 스토리지에서 userId 가져오기
+  const userId = sessionStorage.getItem('user_id') || 'anonymous'; // 로그인하지 않은 경우 'anonymous'
 
-  if (isLoggedIn()) {
+  if (selectedQuantity > 0) {
     try {
-      // 장바구니에 상품을 추가하는 API를 호출합니다.
       const response = await fetch('http://localhost:3001/cart', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(selectedProduct), // 선택한 상품의 정보를 전달합니다.
+        body: JSON.stringify({
+          userId,
+          productId: product.product_id,
+          cart_quantity: selectedQuantity,
+          cart_price: product.price,
+        }),
       });
 
       if (!response.ok) {
@@ -27,16 +30,13 @@ const handleAddToCart = async (product: Product, navigate: NavigateFunction) => 
       }
 
       alert('장바구니에 상품이 추가되었습니다.');
-      navigate('/cart'); // 장바구니 페이지로 이동합니다.
+      navigate('/cart');
     } catch (error) {
       console.error(error);
-      if (error instanceof Error) {
-        // 타입 가드를 사용하여 error의 타입을 확인합니다.
-        alert(error.message); // error가 Error 타입이라는 것을 TypeScript가 알 수 있습니다.
-      }
+      alert('장바구니에 상품을 추가하는데 실패했습니다.');
     }
   } else {
-    navigate('/login');
+    alert('수량을 선택해주세요.');
   }
 };
 
