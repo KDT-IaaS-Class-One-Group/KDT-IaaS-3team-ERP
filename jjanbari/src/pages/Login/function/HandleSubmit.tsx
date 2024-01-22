4e25f411e0e42741a350ef851bc2024043a38fbf
Ho Nyeong <dev.honing@gmail.com>
2024-01-22 15:09:01 +0900

    :recycle: Refactor: role을 이용하지 않도록 개선

--- jjanbari/src/pages/Login/function/HandleSubmit.tsx
+++ jjanbari/src/pages/Login/function/HandleSubmit.tsx
@@ -24,25 +24,14 @@ const handleSubmit = (loginFormData: any, navigate: any) => async (e: FormEvent<
     });
 
     if (response.ok) {
-      const data = await response.json();
+      // 로그인 성공
+      console.log('로그인하였습니다.');
 
-      if (data.role === 'admin') {
-        // 관리자 로그인 성공
-        console.log('관리자로 로그인하였습니다.');
+      // 세션 스토리지에 user_id 저장
+      sessionStorage.setItem('user_id', loginFormData.user_id);
 
-        // 세션 스토리지에 user_id 저장
-        sessionStorage.setItem('user_id', loginFormData.user_id);
-
-        navigate('/admin'); // 관리자 페이지로 이동
-      } else if (data.role === 'user') {
-        // 사용자 로그인 성공
-        console.log('사용자로 로그인하였습니다.');
-
-        // 세션 스토리지에 user_id 저장
-        sessionStorage.setItem('user_id', loginFormData.user_id);
-
-        navigate('/'); // 메인 페이지로 이동
-      }
+      // 메인 페이지로 이동
+      navigate('/');
     } else {
       console.error('로그인 실패:', response.statusText);
       alert('로그인에 실패했습니다. 다시 시도해주세요.');
