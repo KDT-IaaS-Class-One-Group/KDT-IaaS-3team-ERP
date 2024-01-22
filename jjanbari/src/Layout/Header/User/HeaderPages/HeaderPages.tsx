e6af3fce2ae5b99943e20b07e0e528bdace95386
Ho Nyeong <dev.honing@gmail.com>
2024-01-22 13:57:47 +0900

    :sparkles: Feat: 로그인 상태에 따라 로그인/로그아웃을 HeaderPages에 표시

--- jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
+++ jjanbari/src/Layout/Header/User/HeaderPages/HeaderPages.tsx
@@ -1,40 +1,51 @@
 // src/Layout/Header/User/HeaderPages/HeaderPages.tsx
 
 import "./HeaderPages.css";
-import React, { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { isLoggedIn } from "./LoginStatus/isLoggedIn";
+import { useAuth } from "../../../../Auth/AuthContext";
 
 const HeaderPages = () => {
   const navigate = useNavigate();
+  const { isLoggedIn } = useAuth();
+
   return (
     <div className="headerPages">
       <div className="loginStatus">
+        {isLoggedIn ? (
+          // 로그인 상태인 경우
+          <span onClick={() => navigate("/logout")}>로그아웃</span>
+        ) : (
+          // 로그아웃 상태인 경우
+          <Link to="/login">로그인</Link>
+        )}
       </div>
+
       <div className="pages">
         <Link
-          to="/cart"
+          to={isLoggedIn ? "/cart" : "/login"}
           onClick={(event) => {
             event.preventDefault();
-            navigate(isLoggedIn() ? "/cart" : "/login");
+            navigate(isLoggedIn ? "/cart" : "/login");
           }}
         >
           장바구니
         </Link>
         <Link
-          to="/like"
+          to={isLoggedIn ? "/like" : "/login"}
           onClick={(event) => {
             event.preventDefault();
-            navigate(isLoggedIn() ? "/like" : "/login");
+            navigate(isLoggedIn ? "/like" : "/login");
           }}
         >
           좋아요
         </Link>
         <Link
-          to="/mypage"
+          to={isLoggedIn ? "/mypage" : "/login"}
           onClick={(event) => {
             event.preventDefault();
-            navigate(isLoggedIn() ? "/mypage" : "/login");
+            navigate(isLoggedIn ? "/mypage" : "/login");
           }}
         >
           마이페이지
