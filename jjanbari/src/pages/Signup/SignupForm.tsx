4bc437ae542ccc5bbd0b7a30772a175509bfe14b
samuel21695 <samuel21695@gmail.com>
2024-01-25 17:23:30 +0900

    🔨 Modify: modify signup page layout

--- jjanbari/src/pages/Signup/SignupForm.tsx
+++ jjanbari/src/pages/Signup/SignupForm.tsx
@@ -17,20 +17,11 @@ const SignupForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit(User, navigate)} className="SignupForm">
-      <label>
-        아이디:
-        <input type="text" name="user_id" value={User.user_id} onChange={(e) => setUser({ ...User, user_id: e.target.value })} required />
-      </label>
+      <input type="text" name="user_id" value={User.user_id} onChange={(e) => setUser({ ...User, user_id: e.target.value })} required />
       <br />
-      <label>
-        비밀번호:
-        <input type="password" name="user_pw" value={User.user_pw} onChange={(e) => setUser({ ...User, user_pw: e.target.value })} required />
-      </label>
+      <input type="password" name="user_pw" value={User.user_pw} onChange={(e) => setUser({ ...User, user_pw: e.target.value })} required />
       <br />
-      <label>
-        이름:
-        <input type="text" name="user_name" value={User.user_name} onChange={(e) => setUser({ ...User, user_name: e.target.value })} required />
-      </label>
+      <input type="text" name="user_name" value={User.user_name} onChange={(e) => setUser({ ...User, user_name: e.target.value })} required />
       <br />
       <button type="submit">가입하기</button>
     </form>
