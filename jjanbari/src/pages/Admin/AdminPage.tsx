cb579c2db119ea9ce72a39f86157124cecac9d44
samuel21695 <samuel21695@gmail.com>
2024-01-26 11:45:31 +0900

    ✨ Feat: admin Mainpage layouts

--- jjanbari/src/pages/Admin/AdminPage.tsx
+++ jjanbari/src/pages/Admin/AdminPage.tsx
@@ -8,6 +8,16 @@ const AdminPage = () => {
   return (
     <div id="container">
       <h1>관리자 페이지</h1>
+      <div className="statitics-tables">
+        <div className="user-table">
+          <h3>최근 회원 목록</h3>
+
+        </div>
+        <div className="product-table">
+          <h3>최근 등록된 상품</h3>
+
+        </div>
+      </div>
     </div>
   );
 };
