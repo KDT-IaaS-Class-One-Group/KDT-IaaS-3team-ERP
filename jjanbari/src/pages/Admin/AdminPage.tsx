474fe552991e9ebbbb1694cc26126bc4dd4746ae
samuel21695 <samuel21695@gmail.com>
2024-01-26 11:33:13 +0900

    🔨 Modigy: admin page route setting
    statitics -> admin page

--- jjanbari/src/pages/Admin/AdminPage.tsx
+++ jjanbari/src/pages/Admin/AdminPage.tsx
@@ -1,6 +1,5 @@
 // src/pages/Admin/AdminPage.tsx
 import React from "react";
-import { Link } from 'react-router-dom';
 
 
 import './AdminPage.css';
@@ -9,9 +8,6 @@ const AdminPage = () => {
   return (
     <div id="container">
       <h1>관리자 페이지</h1>
-      <Link to="/">
-        <h2>사용자 페이지로 이동</h2>
-      </Link>
     </div>
   );
 };
