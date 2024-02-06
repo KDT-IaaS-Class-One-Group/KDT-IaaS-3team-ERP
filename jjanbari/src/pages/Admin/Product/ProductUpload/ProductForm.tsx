6c6d03e440525ffcd16834fa78941e93a55a3479
ChaeYi <chaeyi990731@gmail.com>
2024-02-06 12:27:31 +0900

    :pencil2: Rename: 가독성을 위한 띄어쓰기

--- jjanbari/src/pages/Admin/Product/ProductUpload/ProductForm.tsx
+++ jjanbari/src/pages/Admin/Product/ProductUpload/ProductForm.tsx
@@ -35,14 +35,16 @@ const ProductForm = () => {
     fetchCategories();
   }, []);
 
+  
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-
+  
     if (!name || price <= 0 || quantity <= 0) {
       alert('상품명, 가격 및 수량을 채워주세요.');
       return;
     }
-
+  
     const productData = {
       name,
       price,
@@ -51,7 +53,7 @@ const ProductForm = () => {
       ageCategory: selectedAgeCategory,
       functionalCategory: selectedFunctionalCategory,
     };
-
+  
     try {
       const response = await fetch('http://localhost:3001/addProductWithImage', {
         method: 'POST',
@@ -60,7 +62,7 @@ const ProductForm = () => {
         },
         body: JSON.stringify(productData),
       });
-
+  
       if (response.ok) {
         alert('상품이 성공적으로 등록되었습니다.');
         navigate('/');
